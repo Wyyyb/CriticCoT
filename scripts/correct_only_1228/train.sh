84b7e4c2ad3713b31ab6c661c423566213b5e6ad
Wyyyb <yubo.wang.sunny@gmail.com>
2024-12-28 18:33:43 +0800

    add

--- scripts/correct_only_1228/train.sh
+++ scripts/correct_only_1228/train.sh
@@ -2,7 +2,7 @@
 
 cd ../../LLaMA-Factory/
 PROJECT_NAME="critic_cot"
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 export WANDB_API_KEY=f318ffd0dcf5d31701fd33aee12e57e9cf15444f
 export WANDB_PROJECT=$PROJECT_NAME
 export WANDB_MODE=disabled
