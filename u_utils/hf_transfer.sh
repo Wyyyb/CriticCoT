2074f0ce4bae53715ff2aeebd8117af395b8f479
Wyyyb <yubo.wang.sunny@gmail.com>
2024-12-28 17:42:19 +0800

    add

--- u_utils/hf_transfer.sh
+++ u_utils/hf_transfer.sh
@@ -1,10 +1,3 @@
-huggingface-cli upload ubowang/scholar-hnsw-1207 . --repo-type dataset
-
-
-huggingface-cli download ubowang/scholar-hnsw-1207 --local-dir . --repo-type dataset
-
-
-
 
 huggingface-cli upload ubowang/CriticCoT_data_1228 . --repo-type dataset
 
