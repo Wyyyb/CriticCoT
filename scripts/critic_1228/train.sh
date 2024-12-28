e506b0713f864ee055048300daa2e3e18956f83c
Wyyyb <yubo.wang.sunny@gmail.com>
2024-12-28 18:21:45 +0800

    add

--- scripts/critic_1228/train.sh
+++ scripts/critic_1228/train.sh
@@ -6,9 +6,9 @@ export CUDA_VISIBLE_DEVICES=0,1
 export WANDB_API_KEY=f318ffd0dcf5d31701fd33aee12e57e9cf15444f
 export WANDB_PROJECT=$PROJECT_NAME
 export WANDB_MODE=disabled
-MASTER_PORT=$(shuf -i 20000-30000 -n 1)
-export MASTER_PORT
-export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+#MASTER_PORT=$(shuf -i 20000-30000 -n 1)
+#export MASTER_PORT
+#export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
 MODEL_NAME="qwen2.5-7b-critic_1228"
 export WANDB_RUN_NAME=$MODEL_NAME
