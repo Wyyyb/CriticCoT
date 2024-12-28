7397bdb4840659b08d6d630271ae2386dfc2ffa1
Wyyyb <yubo.wang.sunny@gmail.com>
2024-12-28 18:43:49 +0800

    add

--- scripts/correct_only_1228/train.sh
+++ scripts/correct_only_1228/train.sh
@@ -4,7 +4,7 @@ source /gpfs/public/research/miniconda3/bin/activate
 conda activate lf_yubo
 cd ../../LLaMA-Factory/
 PROJECT_NAME="critic_cot"
-export CUDA_VISIBLE_DEVICES=0,1,2,3
+export CUDA_VISIBLE_DEVICES=0
 export WANDB_API_KEY=f318ffd0dcf5d31701fd33aee12e57e9cf15444f
 export WANDB_PROJECT=$PROJECT_NAME
 export WANDB_MODE=disabled
