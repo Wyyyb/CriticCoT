ad82766cf4a9e848bbc44f2abd354593746c534c
Wyyyb <yubo.wang.sunny@gmail.com>
2024-12-28 16:39:20 +0800

    add

--- preprocess_webinstruct_data/preprocess_1228.py
+++ preprocess_webinstruct_data/preprocess_1228.py
@@ -40,7 +40,7 @@ def transfer_single(item, mode):
     # index = item.get("index", "")
     gpt4o_answer = item.get("gpt4o_answer", "")
     if gpt4o_answer is None:
-        print("------------------------item", item)
+        return None
     is_correct = parse_single_critic(gpt4o_answer)
     if is_correct is None:
         print("parse_single_critic failed", gpt4o_answer)
@@ -81,20 +81,23 @@ def main():
     training_data = []
     for each in tqdm(input_data):
         curr = transfer_single(each, mode)
-        training_data.append(curr)
+        if curr is not None:
+            training_data.append(curr)
     os.makedirs("../local_data", exist_ok=True)
     os.makedirs("../local_data/training_data_1228", exist_ok=True)
     with open("../local_data/critic_training_data_1228/CriticCoT_correct_only_data_1228.json", "w") as fo:
         fo.write(json.dumps(training_data, indent=2))
-
+    print("length of correct_only data", len(training_data))
     mode = "critic"
     print("creating critic data")
     training_data = []
     for each in tqdm(input_data):
         curr = transfer_single(each, mode)
-        training_data.append(curr)
+        if curr is not None:
+            training_data.append(curr)
     with open("../local_data/critic_training_data_1228/CriticCoT_critic_data_1228.json", "w") as fo:
         fo.write(json.dumps(training_data, indent=2))
+    print("length of critic data", len(training_data))
 
 
 if __name__ == "__main__":
