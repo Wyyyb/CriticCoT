12b2d538d29c1bb8c5814c515f145b65b0a3e11f
Wyyyb <yubo.wang.sunny@gmail.com>
2024-12-31 00:49:59 +0800

    add

---
+++ scripts/eval_1230/eval_ori_1231_darth.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+source /gpfs/public/research/miniconda3/bin/activate
+conda activate lf_yubo
+
+cd /gpfs/public/research/xy/yubowang/CriticCoT/math-evaluation-harness
+
+mkdir -p ../math_eval_result/eval_res_1231_ori/
+
+model_path="/data/yubowang/models/OLMo-2-1124-7B"
+output_dir="../math_eval_result/eval_res_1231_ori/ori_OLMo-2-1124-7B"
+bash scripts/run_eval_math.sh cot $model_path $output_dir
+
+model_path="/data/yubowang/models/deepseek-math-7b-base"
+output_dir="../math_eval_result/eval_res_1231_ori/ori_deepseek-math-7b-base"
+bash scripts/run_eval_math.sh cot $model_path $output_dir
+
+
+
+
--- u_utils/hf_download_models.sh
+++ u_utils/hf_download_models.sh
@@ -2,3 +2,7 @@ huggingface-cli download --repo-type model allenai/OLMo-2-1124-7B --local-dir /g
 
 huggingface-cli download --repo-type model deepseek-ai/deepseek-math-7b-base --local-dir /gpfs/public/research/xy/yubowang/models/deepseek-math-7b-base
 
+
+huggingface-cli download --repo-type model deepseek-ai/deepseek-math-7b-base --local-dir /data/yubowang/models/deepseek-math-7b-base
+
+
