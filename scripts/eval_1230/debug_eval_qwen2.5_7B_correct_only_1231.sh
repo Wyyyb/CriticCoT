69d3f5b7462d951801382be1ca986035295f5e5a
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-01 00:18:07 +0800

    add

--- scripts/eval_1230/debug_eval_qwen2.5_7B_correct_only_1231.sh
+++ scripts/eval_1230/debug_eval_qwen2.5_7B_correct_only_1231.sh
@@ -7,10 +7,15 @@ cd /gpfs/public/research/xy/yubowang/CriticCoT/math-evaluation-harness
 model_dir="/gpfs/public/research/xy/yubowang/CriticCoT/LLaMA-Factory/output_models"
 datasets="math"
 
-model_sub_dir="CriticCoT_correct_only_data_1228/checkpoint-1200"
+model_sub_dir="CriticCoT_correct_only_data_1228_test_1e_5/checkpoint-400"
 model_path="${model_dir}/${model_sub_dir}"
-output_path="../math_eval_result/eval_res_1228/debug_qwen2.5_7B_correct_only_1228_ckpt_1200/"
+output_path="../math_eval_result/eval_res_1228/debug_qwen2.5_7B/"
 bash scripts/run_eval.sh cot $model_path $output_path $datasets
+
+#model_sub_dir="CriticCoT_correct_only_data_1228/checkpoint-1200"
+#model_path="${model_dir}/${model_sub_dir}"
+#output_path="../math_eval_result/eval_res_1228/debug_qwen2.5_7B_correct_only_1228_ckpt_1200/"
+#bash scripts/run_eval.sh cot $model_path $output_path $datasets
 
 #model_sub_dir="CriticCoT_qwq_data_1229/checkpoint-2000"
 #model_path="${model_dir}/${model_sub_dir}"
