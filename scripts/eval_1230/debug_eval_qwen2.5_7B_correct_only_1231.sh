4178ba35de6e8b6b4affd11ced060a03f6749d87
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-01 01:53:36 +0800

    add

--- scripts/eval_1230/debug_eval_qwen2.5_7B_correct_only_1231.sh
+++ scripts/eval_1230/debug_eval_qwen2.5_7B_correct_only_1231.sh
@@ -11,8 +11,8 @@ use_ins="use_ins"
 model_sub_dir="CriticCoT_correct_only_data_1228_test_1e_5/checkpoint-400"
 model_path="${model_dir}/${model_sub_dir}"
 output_path="../math_eval_result/eval_res_1228/debug_qwen2.5_7B/"
-# bash scripts/debug_run_eval.sh cot $model_path $output_path $datasets $use_ins
-bash scripts/run_eval.sh cot $model_path $output_path $datasets $use_ins
+bash scripts/debug_run_eval.sh cot $model_path $output_path $datasets $use_ins
+# bash scripts/run_eval.sh cot $model_path $output_path $datasets $use_ins
 
 #model_sub_dir="CriticCoT_correct_only_data_1228/checkpoint-1200"
 #model_path="${model_dir}/${model_sub_dir}"
