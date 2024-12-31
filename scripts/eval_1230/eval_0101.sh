772113a72cccd951419b7336ec7c52635f423e5e
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-01 01:58:58 +0800

    add

--- scripts/eval_1230/eval_0101.sh
+++ scripts/eval_1230/eval_0101.sh
@@ -9,6 +9,8 @@ cd /gpfs/public/research/xy/yubowang/CriticCoT/math-evaluation-harness
 
 mkdir -p ../math_eval_result/eval_res_0101_ins/
 
+model_dir="/gpfs/public/research/xy/yubowang/CriticCoT/LLaMA-Factory/output_models"
+
 model_sub_dir="CriticCoT_correct_only_data_1228/checkpoint-400"
 model_path="${model_dir}/${model_sub_dir}"
 output_path="../math_eval_result/eval_res_0101_ins/qwen2.5_7B_correct_only_1228_ckpt_400/"
@@ -27,11 +29,6 @@ output_path="../math_eval_result/eval_res_0101_ins/qwen2.5_7B_correct_only_1228_
 bash scripts/run_eval.sh cot $model_path $output_path $datasets $use_ins
 
 
-model_path="/gpfs/public/research/xy/yubowang/models/Qwen2.5-7B"
-output_dir="../math_eval_result/eval_res_0101_ins/ori_qwen2.5_7B/"
-bash scripts/run_eval.sh cot $model_path $output_dir $datasets $use_ins
-
-model_dir="/gpfs/public/research/xy/yubowang/CriticCoT/LLaMA-Factory/output_models"
 model_sub_dir="CriticCoT_critic_data_1228/checkpoint-800"
 model_path="${model_dir}/${model_sub_dir}"
 output_path="../math_eval_result/eval_res_0101_ins/qwen2.5_7B_critic_1228_ckpt_800/"
@@ -46,4 +43,8 @@ model_sub_dir="CriticCoT_critic_data_1228"
 model_path="${model_dir}/${model_sub_dir}"
 output_path="../math_eval_result/eval_res_0101_ins/qwen2.5_7B_critic_1228/"
 bash scripts/run_eval.sh cot $model_path $output_path $datasets $use_ins
+
+model_path="/gpfs/public/research/xy/yubowang/models/Qwen2.5-7B"
+output_dir="../math_eval_result/eval_res_0101_ins/ori_qwen2.5_7B/"
+bash scripts/run_eval.sh cot $model_path $output_dir $datasets $use_ins
 
