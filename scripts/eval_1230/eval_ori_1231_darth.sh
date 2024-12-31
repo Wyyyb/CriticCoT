3f2b3cc95b1be844dfbd8301cd130da8bd636d3a
Wyyyb <yubo.wang.sunny@gmail.com>
2024-12-31 16:33:45 +0800

    add

--- scripts/eval_1230/eval_ori_1231_darth.sh
+++ scripts/eval_1230/eval_ori_1231_darth.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 #source /gpfs/public/research/miniconda3/bin/activate
 #conda activate lf_yubo
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=2
 cd /data/yubowang/CriticCoT/math-evaluation-harness
-datasets="gsm8k,minerva_math"
+#datasets="gsm8k,minerva_math"
+datasets="math"
 
 mkdir -p ../math_eval_result/eval_res_1231_ori/
 
-model_path="/data/yubowang/models/OLMo-2-1124-7B"
-output_dir="../math_eval_result/eval_res_1231_ori/ori_OLMo-2-1124-7B"
-bash scripts/run_eval.sh cot $model_path $output_dir $datasets
+#model_path="/data/yubowang/models/OLMo-2-1124-7B"
+#output_dir="../math_eval_result/eval_res_1231_ori/ori_OLMo-2-1124-7B"
+#bash scripts/run_eval.sh cot $model_path $output_dir $datasets
 
 model_path="/data/yubowang/models/deepseek-math-7b-base"
 output_dir="../math_eval_result/eval_res_1231_ori/ori_deepseek-math-7b-base"
