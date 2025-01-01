775787259bff9c0850fcb1052e6834043a795f0f
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-02 04:52:32 +0800

    add

--- scripts/new_eval_0101/eval_0.sh
+++ scripts/new_eval_0101/eval_0.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -ex
 
 #source /gpfs/public/research/miniconda3/bin/activate
 #conda activate lf_yubo
@@ -7,17 +8,17 @@ export CUDA_VISIBLE_DEVICES=0,1
 cd ../../math_eval
 
 model_sub_dir="output_models/CriticCoT_correct_only_data_1231/checkpoint-723"
-output_dir="../math_eval_result_new/eval_res_0101/qwen2.5_7B_correct_only_1231_ckpt_723/"
+output_dir="../math_eval_result_new/eval_res_0101/qwen2.5_7B_correct_only_1231_ckpt_723"
 bash eval_math.sh $model_sub_dir $output_dir
 
 
 model_sub_dir="output_models/CriticCoT_correct_only_data_1231/checkpoint-1446"
-output_dir="../math_eval_result_new/eval_res_0101/qwen2.5_7B_correct_only_1231_ckpt_1446/"
+output_dir="../math_eval_result_new/eval_res_0101/qwen2.5_7B_correct_only_1231_ckpt_1446"
 bash eval_math.sh $model_sub_dir $output_dir
 
 
 model_sub_dir="output_models/CriticCoT_correct_only_data_1231/checkpoint-2169"
-output_dir="../math_eval_result_new/eval_res_0101/qwen2.5_7B_correct_only_1231_ckpt_2169/"
+output_dir="../math_eval_result_new/eval_res_0101/qwen2.5_7B_correct_only_1231_ckpt_2169"
 bash eval_math.sh $model_sub_dir $output_dir
 
 
