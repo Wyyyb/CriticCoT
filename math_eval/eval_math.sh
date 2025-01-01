92d5e218c2075a9c085d4ecf897ae68f0326a699
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-02 05:07:29 +0800

    add

--- math_eval/eval_math.sh
+++ math_eval/eval_math.sh
@@ -1,13 +1,10 @@
 set -ex
 
-model_sub_dir=$1
+model_path=$1
 output_dir=$2
 
 summary_path="../math_eval_result_new/summary.txt"
 datasets=("math" "gsm8k" "theoremqa" "mmlu_stem" "sat" "svamp")
-model_dir="/gpfs/public/research/xy/yubowang/CriticCoT/LLaMA-Factory"
-
-model_path="${model_dir}/${model_sub_dir}"
 
 for dataset in "${datasets[@]}"; do
     echo "Processing dataset: $dataset"
