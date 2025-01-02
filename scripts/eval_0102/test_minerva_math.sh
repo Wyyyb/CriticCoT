0a73309931576beddc8cfc22147f8449e5e9f173
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-03 02:11:19 +0800

    add

--- scripts/eval_0102/test_minerva_math.sh
+++ scripts/eval_0102/test_minerva_math.sh
@@ -7,6 +7,7 @@ conda activate lf_yubo
 export CUDA_VISIBLE_DEVICES=0,1
 cd ../../math_eval
 
+model_dir="/gpfs/public/research/xy/yubowang/CriticCoT/LLaMA-Factory"
 
 model_sub_dir="output_models/CriticCoT_critic_data_1228/checkpoint-800"
 output_dir="../math_eval_result_new/eval_res_0102/qwen2.5_7B_critic_1228_ckpt_800/"
