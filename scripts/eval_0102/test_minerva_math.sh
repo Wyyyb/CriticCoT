eac186f6aba3fead96d2ba962f907f86df98aca3
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-03 19:47:36 +0800

    add

--- scripts/eval_0102/test_minerva_math.sh
+++ scripts/eval_0102/test_minerva_math.sh
@@ -9,8 +9,23 @@ cd ../../math_eval
 
 model_dir="/gpfs/public/research/xy/yubowang/CriticCoT/LLaMA-Factory"
 
-model_sub_dir="output_models/CriticCoT_critic_data_1228/checkpoint-800"
-output_dir="../math_eval_result_new/eval_res_0102/qwen2.5_7B_critic_1228_ckpt_800/"
-model_path="${model_dir}/${model_sub_dir}"
+#model_sub_dir="output_models/CriticCoT_critic_data_1228/checkpoint-800"
+#output_dir="../math_eval_result_new/eval_res_0102/qwen2.5_7B_critic_1228_ckpt_800/"
+#model_path="${model_dir}/${model_sub_dir}"
+#bash eval_math.sh $model_path $output_dir
+
+
+model_path="/gpfs/public/research/xy/yubowang/models/Qwen2.5-7B"
+output_dir="../math_eval_result_new/eval_res_0102/ori_qwen2.5_7B/"
+bash eval_math.sh $model_path $output_dir
+
+
+model_path="/gpfs/public/research/xy/yubowang/models/Qwen2.5-Math-7B"
+output_dir="../math_eval_result_new/eval_res_0102/ori_qwen2.5_math_7B/"
+bash eval_math.sh $model_path $output_dir
+
+
+model_path="/gpfs/public/research/xy/yubowang/models/deepseek-math-7b-base"
+output_dir="../math_eval_result_new/eval_res_0102/ori_deepseek-math-7b-base/"
 bash eval_math.sh $model_path $output_dir
 
