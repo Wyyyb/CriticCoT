b4ea5c3d9a1f392074f9cdbe9a1f36ccb6fbbfb7
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-05 00:42:17 +0800

    add

---
+++ math_eval/eval_math_v2.sh
@@ -0,0 +1,19 @@
+set -ex
+
+model_path=$1
+output_dir=$2
+
+summary_path="../math_eval_result_new/summary_0105_v2.txt"
+# datasets=("math_500" "math" "gsm8k" "theoremqa" "mmlu_stem" "sat")
+datasets=("math_500" "math")
+
+for dataset in "${datasets[@]}"; do
+    echo "Processing dataset: $dataset"
+    python run_open.py \
+        --model $model_path \
+        --shots 5 \
+        --dataset $dataset \
+        --form few-shot-wo-ins \
+        --output_dir $output_dir \
+        --summary_path $summary_path
+done
