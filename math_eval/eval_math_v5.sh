dd1ef2a25d042e32e8a9cb6486a3727a6b4ef8fc
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-05 01:55:29 +0800

    add

--- math_eval/eval_math_v5.sh
+++ math_eval/eval_math_v5.sh
@@ -3,11 +3,9 @@ set -ex
 model_path=$1
 output_dir=$2
 
-summary_path="../math_eval_result_new/summary_0105_v3.txt"
+summary_path="../math_eval_result_new/summary_0105_v5.txt"
 # datasets=("math_500" "math" "gsm8k" "theoremqa" "mmlu_stem" "sat")
-#datasets=("math_500" "math")
-datasets=("aime" "OlympiadBench" "aime_24")
-# datasets=("aime_24")
+datasets=("math" "math_500")
 
 for dataset in "${datasets[@]}"; do
     echo "Processing dataset: $dataset"
