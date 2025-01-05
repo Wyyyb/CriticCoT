0b118604faf053f3499f4e1c08474bed4d7052f5
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-05 21:10:26 +0800

    add

--- math_eval/eval_math_test_0105.sh
+++ math_eval/eval_math_test_0105.sh
@@ -4,16 +4,16 @@ model_path=$1
 output_dir=$2
 summary_path=$3
 
-cd /gpfs/public/research/xy/yubowang/CriticCoT/MMLU-Pro
-bash mmlu-pro-eval.sh $model_path $output_dir $summary_path
-cd /gpfs/public/research/xy/yubowang/CriticCoT/math_eval
+#cd /gpfs/public/research/xy/yubowang/CriticCoT/MMLU-Pro
+#bash mmlu-pro-eval.sh $model_path $output_dir $summary_path
+#cd /gpfs/public/research/xy/yubowang/CriticCoT/math_eval
 
 # datasets=("math_500" "math" "gsm8k" "theoremqa" "mmlu_stem" "sat" "aime" "aime_24" "OlympiadBench")
 datasets=("gsm8k" "math_500" "math")
 
 for dataset in "${datasets[@]}"; do
     echo "Processing dataset: $dataset"
-    python run_open.py \
+    python run_open_test_0105.py \
         --model $model_path \
         --shots 5 \
         --dataset $dataset \
