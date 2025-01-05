1749d937407e735413cc002960411c1c335f02eb
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-05 21:08:29 +0800

    add

--- scripts/eval_0105/eval_0105_test.sh
+++ scripts/eval_0105/eval_0105_test.sh
@@ -13,11 +13,11 @@ model_dir="/gpfs/public/research/xy/yubowang/CriticCoT/LLaMA-Factory"
 
 
 model_path="/gpfs/public/research/xy/yubowang/models/Qwen2.5-7B"
-output_dir="../math_eval_result_v1_job/eval_res_0105/ori_qwen2.5_7B/"
+output_dir="../math_eval_result_v1_test/eval_res_0105/ori_qwen2.5_7B/"
 bash eval_math_test_0105.sh $model_path $output_dir $summary_path
 
 
 model_path="/gpfs/public/research/xy/yubowang/models/Qwen2.5-Math-7B"
-output_dir="../math_eval_result_v1_job/eval_res_0105/ori_qwen2.5_math_7B/"
+output_dir="../math_eval_result_v1_test/eval_res_0105/ori_qwen2.5_math_7B/"
 bash eval_math_test_0105.sh $model_path $output_dir $summary_path
 
