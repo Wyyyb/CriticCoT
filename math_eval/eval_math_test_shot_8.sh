810c210a9853cac077101d689bc872a93a49c7e8
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-06 02:46:26 +0800

    add

---
+++ math_eval/eval_math_test_shot_8.sh
@@ -0,0 +1,24 @@
+set -ex
+
+model_path=$1
+output_dir=$2
+summary_path=$3
+
+#cd /gpfs/public/research/xy/yubowang/CriticCoT/MMLU-Pro
+#bash mmlu-pro-eval.sh $model_path $output_dir $summary_path
+#cd /gpfs/public/research/xy/yubowang/CriticCoT/math_eval
+
+# datasets=("math_500" "math" "gsm8k" "theoremqa" "mmlu_stem" "sat" "aime" "aime_24" "OlympiadBench")
+datasets=("math_500" "math" "gsm8k")
+
+for dataset in "${datasets[@]}"; do
+    echo "Processing dataset: $dataset"
+    python run_open_test_shot_8.py \
+        --model $model_path \
+        --shots 5 \
+        --dataset $dataset \
+        --form few-shot \
+        --output_dir $output_dir \
+        --summary_path $summary_path
+done
+
