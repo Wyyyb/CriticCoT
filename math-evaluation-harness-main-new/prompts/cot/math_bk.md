fa97c717207ed6c15ce80bba4f1e2fdcb115e560
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-08 23:08:15 +0800

    add

--- math-evaluation-harness-main-new/prompts/cot/math_bk.md
+++ math-evaluation-harness-main-new/prompts/cot/math_bk.md
@@ -1,7 +1,7 @@
 Problem:
 Find the domain of the expression $\\frac{\\sqrt{x-2}}{\\sqrt{5-x}}$.}
 Solution:
-Let's think step by step. The expressions inside each square root must be non-negative.
+The expressions inside each square root must be non-negative.
 Therefore, $x-2 \\ge 0$, so $x\\ge2$, and $5 - x \\ge 0$, so $x \\le 5$.
 Also, the denominator cannot be equal to zero, so $5-x>0$, which gives $x<5$.
 Therefore, the domain of the expression is $\\boxed{[2,5)}$.
@@ -11,14 +11,14 @@ Final Answer: The final answer is $[2,5)$. I hope it is correct.
 Problem:
 If $\\det \\mathbf{A} = 2$ and $\\det \\mathbf{B} = 12,$ then find $\\det (\\mathbf{A} \\mathbf{B}).$
 Solution:
-Let's think step by step. We have that $\\det (\\mathbf{A} \\mathbf{B}) = (\\det \\mathbf{A})(\\det \\mathbf{B}) = (2)(12) = \\boxed{24}.$
+We have that $\\det (\\mathbf{A} \\mathbf{B}) = (\\det \\mathbf{A})(\\det \\mathbf{B}) = (2)(12) = \\boxed{24}.$
 Final Answer: The final answer is $24$. I hope it is correct.
 
 
 Problem:
 Terrell usually lifts two 20-pound weights 12 times. If he uses two 15-pound weights instead, how many times must Terrell lift them in order to lift the same total weight?
 Solution:
-Let's think step by step. If Terrell lifts two 20-pound weights 12 times, he lifts a total of $2\\cdot 12\\cdot20=480$ pounds of weight.  If he lifts two 15-pound weights instead for $n$ times, he will lift a total of $2\\cdot15\\cdot n=30n$ pounds of weight.  Equating this to 480 pounds, we can solve for $n$: \\begin{align*}
+If Terrell lifts two 20-pound weights 12 times, he lifts a total of $2\\cdot 12\\cdot20=480$ pounds of weight.  If he lifts two 15-pound weights instead for $n$ times, he will lift a total of $2\\cdot15\\cdot n=30n$ pounds of weight.  Equating this to 480 pounds, we can solve for $n$: \\begin{align*}
 30n&=480\\\\
 \\Rightarrow\\qquad n&=480/30=\\boxed{16}
 \\end{align*}
@@ -33,7 +33,7 @@ If the system of equations
 6y-9x &=b.
 \\end{align*}has a solution $(x, y)$ where $x$ and $y$ are both nonzero, find $\\frac{a}{b},$ assuming $b$ is nonzero.
 Solution:
-Let's think step by step. If we multiply the first equation by $-\\frac{3}{2}$, we obtain
+If we multiply the first equation by $-\\frac{3}{2}$, we obtain
 
 $$6y-9x=-\\frac{3}{2}a.$$Since we also know that $6y-9x=b$, we have
 
---
+++ scripts/harness_eval_0108/eval_0108_v3.sh
@@ -0,0 +1,143 @@
+#!/bin/bash
+set -ex
+
+source /gpfs/public/research/miniconda3/bin/activate
+conda activate lf_yubo
+
+export CUDA_VISIBLE_DEVICES=0
+cd /gpfs/public/research/xy/yubowang/CriticCoT/math-evaluation-harness-main-new/scripts
+
+
+summary_path="../harness_math_eval_result_0108_v3/summary_0108_v3.txt"
+model_dir="/gpfs/public/research/xy/yubowang/CriticCoT/LLaMA-Factory"
+
+
+#model_path="/gpfs/public/research/xy/yubowang/models/Qwen2.5-7B"
+#output_dir="../harness_math_eval_result_0108_v2/qwen_eval_res_0108_new_math/ori_qwen2.5_7B/"
+#bash run_eval_0108.sh $model_path $output_dir $summary_path
+#
+#
+#model_path="/gpfs/public/research/xy/yubowang/models/Qwen2.5-Math-7B"
+#output_dir="../harness_math_eval_result_0108_v2/qwen_eval_res_0108_new_math/ori_qwen2.5_math_7B/"
+#bash run_eval_0108.sh $model_path $output_dir $summary_path
+
+
+#model_sub_dir="qwen_math_output_models/CriticCoT_critic_data_1231/checkpoint-1171"
+#output_dir="../harness_math_eval_result_0108_v2/qwen_eval_res_0108_new_math/qwen2.5_math_7B_critic_1231_ckpt_1171/"
+#model_path="${model_dir}/${model_sub_dir}"
+#bash run_eval_0108.sh $model_path $output_dir $summary_path
+
+
+model_sub_dir="qwen_math_output_models/CriticCoT_critic_data_1231/checkpoint-2342"
+output_dir="../harness_math_eval_result_0108_v2/qwen_eval_res_0108_new_math/qwen2.5_math_7B_critic_1231_ckpt_2342/"
+model_path="${model_dir}/${model_sub_dir}"
+bash run_eval_0108.sh $model_path $output_dir $summary_path
+
+
+#model_sub_dir="qwen_math_output_models/CriticCoT_critic_data_1231/checkpoint-3513"
+#output_dir="../harness_math_eval_result_0108_v2/qwen_eval_res_0108_new_math/qwen2.5_math_7B_critic_1231_ckpt_3513/"
+#model_path="${model_dir}/${model_sub_dir}"
+#bash run_eval_0108.sh $model_path $output_dir $summary_path
+
+
+#model_sub_dir="qwen_math_output_models/CriticCoT_critic_add_50k_correct_data_1231/checkpoint-1269"
+#output_dir="../harness_math_eval_result_0108_v2/qwen_eval_res_0108_new_math/qwen2.5_math_7B_critic_add_50k_correct_1231_ckpt_1269/"
+#model_path="${model_dir}/${model_sub_dir}"
+#bash run_eval_0108.sh $model_path $output_dir $summary_path
+#
+
+model_sub_dir="qwen_math_output_models/CriticCoT_critic_add_50k_correct_data_1231/checkpoint-2538"
+output_dir="../harness_math_eval_result_0108_v2/qwen_eval_res_0108_new_math/qwen2.5_math_7B_critic_add_50k_correct_1231_ckpt_2538/"
+model_path="${model_dir}/${model_sub_dir}"
+bash run_eval_0108.sh $model_path $output_dir $summary_path
+
+#
+#model_sub_dir="qwen_math_output_models/CriticCoT_critic_add_50k_correct_data_1231/checkpoint-3807"
+#output_dir="../harness_math_eval_result_0108_v2/qwen_eval_res_0108_new_math/qwen2.5_math_7B_critic_add_50k_correct_1231_ckpt_3807/"
+#model_path="${model_dir}/${model_sub_dir}"
+#bash run_eval_0108.sh $model_path $output_dir $summary_path
+
+
+#model_sub_dir="qwen_math_output_models/CriticCoT_correct_only_data_1231/checkpoint-723"
+#output_dir="../harness_math_eval_result_0108_v2/qwen_eval_res_0108_new_math/qwen2.5_math_7B_correct_only_1231_ckpt_723"
+#model_path="${model_dir}/${model_sub_dir}"
+#bash run_eval_0108.sh $model_path $output_dir $summary_path
+
+
+model_sub_dir="qwen_math_output_models/CriticCoT_correct_only_data_1231/checkpoint-1446"
+output_dir="../harness_math_eval_result_0108_v2/qwen_eval_res_0108_new_math/qwen2.5_math_7B_correct_only_1231_ckpt_1446"
+model_path="${model_dir}/${model_sub_dir}"
+bash run_eval_0108.sh $model_path $output_dir $summary_path
+
+
+#model_sub_dir="qwen_math_output_models/CriticCoT_correct_only_data_1231/checkpoint-2169"
+#output_dir="../harness_math_eval_result_0108_v2/qwen_eval_res_0108_new_math/qwen2.5_math_7B_correct_only_1231_ckpt_2169"
+#model_path="${model_dir}/${model_sub_dir}"
+#bash run_eval_0108.sh $model_path $output_dir $summary_path
+
+
+#model_sub_dir="output_models/CriticCoT_critic_data_1231/checkpoint-1171"
+#output_dir="../harness_math_eval_result_0108_v2/qwen_eval_res_0108_new_math/qwen2.5_7B_critic_1231_ckpt_1171/"
+#model_path="${model_dir}/${model_sub_dir}"
+#bash run_eval_0108.sh $model_path $output_dir $summary_path
+#
+
+model_sub_dir="output_models/CriticCoT_critic_data_1231/checkpoint-2342"
+output_dir="../harness_math_eval_result_0108_v2/qwen_eval_res_0108_new_math/qwen2.5_7B_critic_1231_ckpt_2342/"
+model_path="${model_dir}/${model_sub_dir}"
+bash run_eval_0108.sh $model_path $output_dir $summary_path
+
+
+#model_sub_dir="output_models/CriticCoT_critic_data_1231/checkpoint-3513"
+#output_dir="../harness_math_eval_result_0108_v2/qwen_eval_res_0108_new_math/qwen2.5_7B_critic_1231_ckpt_3513/"
+#model_path="${model_dir}/${model_sub_dir}"
+#bash run_eval_0108.sh $model_path $output_dir $summary_path
+
+
+#model_sub_dir="output_models/CriticCoT_critic_add_50k_correct_data_1231/checkpoint-1269"
+#output_dir="../harness_math_eval_result_0108_v2/qwen_eval_res_0108_new_math/qwen2.5_7B_critic_add_50k_correct_1231_ckpt_1269/"
+#model_path="${model_dir}/${model_sub_dir}"
+#bash run_eval_0108.sh $model_path $output_dir $summary_path
+#
+
+model_sub_dir="output_models/CriticCoT_critic_add_50k_correct_data_1231/checkpoint-2538"
+output_dir="../harness_math_eval_result_0108_v2/qwen_eval_res_0108_new_math/qwen2.5_7B_critic_add_50k_correct_1231_ckpt_2538/"
+model_path="${model_dir}/${model_sub_dir}"
+bash run_eval_0108.sh $model_path $output_dir $summary_path
+
+
+#model_sub_dir="output_models/CriticCoT_critic_add_50k_correct_data_1231/checkpoint-3807"
+#output_dir="../harness_math_eval_result_0108_v2/qwen_eval_res_0108_new_math/qwen2.5_7B_critic_add_50k_correct_1231_ckpt_3807/"
+#model_path="${model_dir}/${model_sub_dir}"
+#bash run_eval_0108.sh $model_path $output_dir $summary_path
+
+
+#model_sub_dir="output_models/CriticCoT_correct_only_data_1231/checkpoint-723"
+#output_dir="../harness_math_eval_result_0108_v2/qwen_eval_res_0108_new_math/qwen2.5_7B_correct_only_1231_ckpt_723"
+#model_path="${model_dir}/${model_sub_dir}"
+#bash run_eval_0108.sh $model_path $output_dir $summary_path
+
+
+model_sub_dir="output_models/CriticCoT_correct_only_data_1231/checkpoint-1446"
+output_dir="../harness_math_eval_result_0108_v2/qwen_eval_res_0108_new_math/qwen2.5_7B_correct_only_1231_ckpt_1446"
+model_path="${model_dir}/${model_sub_dir}"
+bash run_eval_0108.sh $model_path $output_dir $summary_path
+
+
+#model_sub_dir="output_models/CriticCoT_correct_only_data_1231/checkpoint-2169"
+#output_dir="../harness_math_eval_result_0108_v2/qwen_eval_res_0108_new_math/qwen2.5_7B_correct_only_1231_ckpt_2169"
+#model_path="${model_dir}/${model_sub_dir}"
+#bash run_eval_0108.sh $model_path $output_dir $summary_path
+
+
+#model_sub_dir="output_models/CriticCoT_qwq_critic_data_1229/checkpoint-8000"
+#output_dir="../harness_math_eval_result_0108_v2/qwen_eval_res_0108_new_math/qwen2.5_7B_qwq_critic_1229_ckpt_8000/"
+#model_path="${model_dir}/${model_sub_dir}"
+#bash run_eval_0108.sh $model_path $output_dir $summary_path
+#
+#
+#model_sub_dir="output_models/CriticCoT_qwq_data_1229/checkpoint-8000"
+#output_dir="../harness_math_eval_result_0108_v2/qwen_eval_res_0108_new_math/qwen2.5_7B_qwq_1229_ckpt_8000/"
+#model_path="${model_dir}/${model_sub_dir}"
+#bash run_eval_0108.sh $model_path $output_dir $summary_path
+
