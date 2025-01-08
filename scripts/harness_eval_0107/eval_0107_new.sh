f1130795ce9beb24fa73f9789e588fc87d36b93f
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-08 08:56:55 +0800

    add

--- scripts/harness_eval_0107/eval_0107_new.sh
+++ scripts/harness_eval_0107/eval_0107_new.sh
@@ -8,136 +8,68 @@ export CUDA_VISIBLE_DEVICES=1
 cd /gpfs/public/research/xy/yubowang/CriticCoT/math-evaluation-harness-main-new/scripts
 
 
-summary_path="../harness_math_eval_result_0107_new_math/summary_0107_new_math.txt"
+summary_path="../harness_math_eval_result_0108_part2_dev/summary_0108_part2.txt"
 model_dir="/gpfs/public/research/xy/yubowang/CriticCoT/LLaMA-Factory"
 
 
-model_path="/gpfs/public/research/xy/yubowang/models/Qwen2.5-7B"
-output_dir="../harness_math_eval_result_0107_new_math/qwen_eval_res_0107/ori_qwen2.5_7B/"
-bash run_eval_0107.sh $model_path $output_dir $summary_path
+model_path="/gpfs/public/research/xy/yubowang/models/Qwen2.5-Math-7B-Instruct"
+output_dir="../harness_math_eval_result_0108_part2_dev/eval_res_0108_new_math/ori_Qwen2.5-Math-7B-Instruct/"
+bash run_eval_0108.sh $model_path $output_dir $summary_path
 
 
-model_path="/gpfs/public/research/xy/yubowang/models/Qwen2.5-Math-7B"
-output_dir="../harness_math_eval_result_0107_new_math/qwen_eval_res_0107/ori_qwen2.5_math_7B/"
-bash run_eval_0107.sh $model_path $output_dir $summary_path
-
-
-model_sub_dir="qwen_math_output_models/CriticCoT_critic_data_1231/checkpoint-1171"
-output_dir="../harness_math_eval_result_0107_new_math/qwen_eval_res_0107/qwen2.5_math_7B_critic_1231_ckpt_1171/"
+model_sub_dir="output_models/CriticCoT_critic_data_1228/checkpoint-800"
+output_dir="../harness_math_eval_result_0108_part2_dev/eval_res_0108_new_math/qwen2.5_7B_critic_1228_ckpt_800/"
 model_path="${model_dir}/${model_sub_dir}"
-bash run_eval_0107.sh $model_path $output_dir $summary_path
+bash run_eval_0108.sh $model_path $output_dir $summary_path
 
 
-model_sub_dir="qwen_math_output_models/CriticCoT_critic_data_1231/checkpoint-2342"
-output_dir="../harness_math_eval_result_0107_new_math/qwen_eval_res_0107/qwen2.5_math_7B_critic_1231_ckpt_2342/"
+model_sub_dir="output_models/CriticCoT_critic_data_1228/checkpoint-1600"
+output_dir="../harness_math_eval_result_0108_part2_dev/eval_res_0108_new_math/qwen2.5_7B_critic_1228_ckpt_1600/"
 model_path="${model_dir}/${model_sub_dir}"
-bash run_eval_0107.sh $model_path $output_dir $summary_path
+bash run_eval_0108.sh $model_path $output_dir $summary_path
 
 
-model_sub_dir="qwen_math_output_models/CriticCoT_critic_data_1231/checkpoint-3513"
-output_dir="../harness_math_eval_result_0107_new_math/qwen_eval_res_0107/qwen2.5_math_7B_critic_1231_ckpt_3513/"
+model_sub_dir="output_models/CriticCoT_critic_data_1228/checkpoint-2340"
+output_dir="../harness_math_eval_result_0108_part2_dev/eval_res_0108_new_math/qwen2.5_7B_critic_1228_ckpt_2340/"
 model_path="${model_dir}/${model_sub_dir}"
-bash run_eval_0107.sh $model_path $output_dir $summary_path
+bash run_eval_0108.sh $model_path $output_dir $summary_path
 
 
-model_sub_dir="qwen_math_output_models/CriticCoT_critic_add_50k_correct_data_1231/checkpoint-1269"
-output_dir="../harness_math_eval_result_0107_new_math/qwen_eval_res_0107/qwen2.5_math_7B_critic_add_50k_correct_1231_ckpt_1269/"
+model_sub_dir="output_models/CriticCoT_qwq_critic_data_1229/checkpoint-8000"
+output_dir="../harness_math_eval_result_0108_part2_dev/eval_res_0108_new_math/qwen2.5_7B_qwq_critic_1229_ckpt_8000/"
 model_path="${model_dir}/${model_sub_dir}"
-bash run_eval_0107.sh $model_path $output_dir $summary_path
+bash run_eval_0108.sh $model_path $output_dir $summary_path
 
 
-model_sub_dir="qwen_math_output_models/CriticCoT_critic_add_50k_correct_data_1231/checkpoint-2538"
-output_dir="../harness_math_eval_result_0107_new_math/qwen_eval_res_0107/qwen2.5_math_7B_critic_add_50k_correct_1231_ckpt_2538/"
+model_sub_dir="output_models/CriticCoT_qwq_data_1229/checkpoint-8000"
+output_dir="../harness_math_eval_result_0108_part2_dev/eval_res_0108_new_math/qwen2.5_7B_qwq_1229_ckpt_8000/"
 model_path="${model_dir}/${model_sub_dir}"
-bash run_eval_0107.sh $model_path $output_dir $summary_path
+bash run_eval_0108.sh $model_path $output_dir $summary_path
 
 
-model_sub_dir="qwen_math_output_models/CriticCoT_critic_add_50k_correct_data_1231/checkpoint-3807"
-output_dir="../harness_math_eval_result_0107_new_math/qwen_eval_res_0107/qwen2.5_math_7B_critic_add_50k_correct_1231_ckpt_3807/"
+model_path="/gpfs/public/research/xy/yubowang/models/deepseek-math-7b-base"
+output_dir="../harness_math_eval_result_0108_part2_dev/eval_res_0108_new_math/ori_deepseek-math-7b-base/"
+bash run_eval_0108.sh $model_path $output_dir $summary_path
+
+
+model_path="/gpfs/public/research/xy/yubowang/models/Mathstral-7B-v0.1"
+output_dir="../harness_math_eval_result_0108_part2_dev/eval_res_0108_new_math/ori_Mathstral-7B-v0.1/"
+bash run_eval_0108.sh $model_path $output_dir $summary_path
+
+
+model_sub_dir="output_models/CriticCoT_correct_only_data_1228/checkpoint-400"
+output_dir="../harness_math_eval_result_0108_part2_dev/eval_res_0108_new_math/qwen2.5_7B_correct_only_1228_ckpt_400/"
 model_path="${model_dir}/${model_sub_dir}"
-bash run_eval_0107.sh $model_path $output_dir $summary_path
+bash run_eval_0108.sh $model_path $output_dir $summary_path
 
 
-model_sub_dir="qwen_math_output_models/CriticCoT_correct_only_data_1231/checkpoint-723"
-output_dir="../harness_math_eval_result_0107_new_math/qwen_eval_res_0107/qwen2.5_math_7B_correct_only_1231_ckpt_723"
+model_sub_dir="output_models/CriticCoT_correct_only_data_1228/checkpoint-800"
+output_dir="../harness_math_eval_result_0108_part2_dev/eval_res_0108_new_math/qwen2.5_7B_correct_only_1228_ckpt_800/"
 model_path="${model_dir}/${model_sub_dir}"
-bash run_eval_0107.sh $model_path $output_dir $summary_path
+bash run_eval_0108.sh $model_path $output_dir $summary_path
 
 
-model_sub_dir="qwen_math_output_models/CriticCoT_correct_only_data_1231/checkpoint-1446"
-output_dir="../harness_math_eval_result_0107_new_math/qwen_eval_res_0107/qwen2.5_math_7B_correct_only_1231_ckpt_1446"
+model_sub_dir="output_models/CriticCoT_correct_only_data_1228/checkpoint-1200"
+output_dir="../harness_math_eval_result_0108_part2_dev/eval_res_0108_new_math/qwen2.5_7B_correct_only_1228_ckpt_1200/"
 model_path="${model_dir}/${model_sub_dir}"
-bash run_eval_0107.sh $model_path $output_dir $summary_path
-
-
-model_sub_dir="qwen_math_output_models/CriticCoT_correct_only_data_1231/checkpoint-2169"
-output_dir="../harness_math_eval_result_0107_new_math/qwen_eval_res_0107/qwen2.5_math_7B_correct_only_1231_ckpt_2169"
-model_path="${model_dir}/${model_sub_dir}"
-bash run_eval_0107.sh $model_path $output_dir $summary_path
-
-
-model_sub_dir="output_models/CriticCoT_critic_data_1231/checkpoint-1171"
-output_dir="../harness_math_eval_result_0107_new_math/qwen_eval_res_0107/qwen2.5_7B_critic_1231_ckpt_1171/"
-model_path="${model_dir}/${model_sub_dir}"
-bash run_eval_0107.sh $model_path $output_dir $summary_path
-
-
-model_sub_dir="output_models/CriticCoT_critic_data_1231/checkpoint-2342"
-output_dir="../harness_math_eval_result_0107_new_math/qwen_eval_res_0107/qwen2.5_7B_critic_1231_ckpt_2342/"
-model_path="${model_dir}/${model_sub_dir}"
-bash run_eval_0107.sh $model_path $output_dir $summary_path
-
-
-model_sub_dir="output_models/CriticCoT_critic_data_1231/checkpoint-3513"
-output_dir="../harness_math_eval_result_0107_new_math/qwen_eval_res_0107/qwen2.5_7B_critic_1231_ckpt_3513/"
-model_path="${model_dir}/${model_sub_dir}"
-bash run_eval_0107.sh $model_path $output_dir $summary_path
-
-
-model_sub_dir="output_models/CriticCoT_critic_add_50k_correct_data_1231/checkpoint-1269"
-output_dir="../harness_math_eval_result_0107_new_math/qwen_eval_res_0107/qwen2.5_7B_critic_add_50k_correct_1231_ckpt_1269/"
-model_path="${model_dir}/${model_sub_dir}"
-bash run_eval_0107.sh $model_path $output_dir $summary_path
-
-
-model_sub_dir="output_models/CriticCoT_critic_add_50k_correct_data_1231/checkpoint-2538"
-output_dir="../harness_math_eval_result_0107_new_math/qwen_eval_res_0107/qwen2.5_7B_critic_add_50k_correct_1231_ckpt_2538/"
-model_path="${model_dir}/${model_sub_dir}"
-bash run_eval_0107.sh $model_path $output_dir $summary_path
-
-
-model_sub_dir="output_models/CriticCoT_critic_add_50k_correct_data_1231/checkpoint-3807"
-output_dir="../harness_math_eval_result_0107_new_math/qwen_eval_res_0107/qwen2.5_7B_critic_add_50k_correct_1231_ckpt_3807/"
-model_path="${model_dir}/${model_sub_dir}"
-bash run_eval_0107.sh $model_path $output_dir $summary_path
-
-
-model_sub_dir="output_models/CriticCoT_correct_only_data_1231/checkpoint-723"
-output_dir="../harness_math_eval_result_0107_new_math/qwen_eval_res_0107/qwen2.5_7B_correct_only_1231_ckpt_723"
-model_path="${model_dir}/${model_sub_dir}"
-bash run_eval_0107.sh $model_path $output_dir $summary_path
-
-
-model_sub_dir="output_models/CriticCoT_correct_only_data_1231/checkpoint-1446"
-output_dir="../harness_math_eval_result_0107_new_math/qwen_eval_res_0107/qwen2.5_7B_correct_only_1231_ckpt_1446"
-model_path="${model_dir}/${model_sub_dir}"
-bash run_eval_0107.sh $model_path $output_dir $summary_path
-
-
-model_sub_dir="output_models/CriticCoT_correct_only_data_1231/checkpoint-2169"
-output_dir="../harness_math_eval_result_0107_new_math/qwen_eval_res_0107/qwen2.5_7B_correct_only_1231_ckpt_2169"
-model_path="${model_dir}/${model_sub_dir}"
-bash run_eval_0107.sh $model_path $output_dir $summary_path
-
-
-#model_sub_dir="output_models/CriticCoT_qwq_critic_data_1229/checkpoint-8000"
-#output_dir="../harness_math_eval_result_0107_new_math/qwen_eval_res_0107/qwen2.5_7B_qwq_critic_1229_ckpt_8000/"
-#model_path="${model_dir}/${model_sub_dir}"
-#bash run_eval_0107.sh $model_path $output_dir $summary_path
-#
-#
-#model_sub_dir="output_models/CriticCoT_qwq_data_1229/checkpoint-8000"
-#output_dir="../harness_math_eval_result_0107_new_math/qwen_eval_res_0107/qwen2.5_7B_qwq_1229_ckpt_8000/"
-#model_path="${model_dir}/${model_sub_dir}"
-#bash run_eval_0107.sh $model_path $output_dir $summary_path
-
+bash run_eval_0108.sh $model_path $output_dir $summary_path
