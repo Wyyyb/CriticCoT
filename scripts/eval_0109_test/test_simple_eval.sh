370a65c12aa9a17de930f090087fa74a7c6703ab
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-09 16:59:33 +0800

    add

--- scripts/eval_0109_test/test_simple_eval.sh
+++ scripts/eval_0109_test/test_simple_eval.sh
@@ -12,6 +12,7 @@ cd ../../
 #model_sub_dir="qwen_math_output_models/CriticCoT_critic_data_1231/checkpoint-1171"
 #model_path="${model_dir}/${model_sub_dir}"
 model_path="Qwen/Qwen2.5-Math-7B-Instruct"
+model_name="gpt-4o-2024-11-20_chatgpt"
 
-python -m simple-evals.simple_evals --model $model_path --examples 500
+python -m simple-evals.simple_evals --model $model_name --examples 500
 
