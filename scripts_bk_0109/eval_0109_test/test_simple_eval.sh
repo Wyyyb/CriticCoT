35fa74571c0dd5ec3a0c43acb553e11b84f2ed15
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-10 02:22:34 +0800

    add

--- scripts_bk_0109/eval_0109_test/test_simple_eval.sh
+++ scripts_bk_0109/eval_0109_test/test_simple_eval.sh
@@ -7,6 +7,7 @@ conda activate lf_yubo
 cd ../../
 
 # python -m simple-evals.simple_evals --list-models
+export OPENAI_API_KEY="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"
 
 #model_dir="/gpfs/public/research/xy/yubowang/CriticCoT/LLaMA-Factory"
 #model_sub_dir="qwen_math_output_models/CriticCoT_critic_data_1231/checkpoint-1171"
