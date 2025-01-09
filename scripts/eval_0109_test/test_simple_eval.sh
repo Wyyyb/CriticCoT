42110abc9fe38e7a42b2f53c0b176498589ecc77
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-09 16:45:55 +0800

    add

--- scripts/eval_0109_test/test_simple_eval.sh
+++ scripts/eval_0109_test/test_simple_eval.sh
@@ -4,6 +4,8 @@ set -ex
 source /gpfs/public/research/miniconda3/bin/activate
 conda activate lf_yubo
 
+export OPENAI_API_KEY=sk-proj-Smk0AMd6n3LRT3g585BJT3BlbkFJC4lheINPeNTfg6ucK9b0
+
 cd ../../
 
 python -m simple-evals.simple_evals --list-models
