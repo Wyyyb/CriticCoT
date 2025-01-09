dd5d001e972787d853029c4371d15ab9eaa73555
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-09 16:40:41 +0800

    add

--- scripts/eval_0109_test/test_simple_eval.sh
+++ scripts/eval_0109_test/test_simple_eval.sh
@@ -4,6 +4,6 @@ set -ex
 source /gpfs/public/research/miniconda3/bin/activate
 conda activate lf_yubo
 
-cd ../../simple-evals-main
+cd ../../
 
-python simple_evals.py --list-models
+python -m simple-evals.simple_evals --list-models
