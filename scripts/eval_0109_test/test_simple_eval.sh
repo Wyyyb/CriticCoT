23cf8a9b73a97e193feb0f4e7788e66173c4870a
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-09 16:54:20 +0800

    add

--- scripts/eval_0109_test/test_simple_eval.sh
+++ scripts/eval_0109_test/test_simple_eval.sh
@@ -12,5 +12,5 @@ model_dir="/gpfs/public/research/xy/yubowang/CriticCoT/LLaMA-Factory"
 model_sub_dir="qwen_math_output_models/CriticCoT_critic_data_1231/checkpoint-1171"
 model_path="${model_dir}/${model_sub_dir}"
 
-python -m simple-evals.simple_evals --model $model_path --examples <num_examples>
+python -m simple-evals.simple_evals --model $model_path --examples 500
 
