9dc2fbf111802c164644fe589546784e3bd59b9c
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-10 15:07:34 +0800

    add

--- scripts/eval_0110/eval_0110_part_0.sh
+++ scripts/eval_0110/eval_0110_part_0.sh
@@ -11,20 +11,20 @@ cd /gpfs/public/research/xy/yubowang/CriticCoT/Qwen2.5-Math-Eval/evaluation/sh
 summary_path="../math_eval_result_0110/summary_0110.txt"
 model_dir="/gpfs/public/research/xy/yubowang/CriticCoT/LLaMA-Factory/output_models_bk"
 
-#
-#model_path="/gpfs/public/research/xy/yubowang/models/Qwen2.5-7B"
-#output_dir="../math_eval_result_0110/qwen_eval_res_0110_new_math/ori_qwen2.5_7B/"
-#bash eval_math.sh $model_path $output_dir $summary_path
-#
-#
-#model_path="/gpfs/public/research/xy/yubowang/models/Qwen2.5-Math-7B"
-#output_dir="../math_eval_result_0110/qwen_eval_res_0110_new_math/ori_qwen2.5_math_7B/"
-#bash eval_math.sh $model_path $output_dir $summary_path
-#
-#
-#model_path="/gpfs/public/research/xy/yubowang/models/Qwen2.5-Math-7B-Instruct"
-#output_dir="../math_eval_result_0110/qwen_eval_res_0110_new_math/ori_qwen2.5_math_7B_instruct/"
-#bash eval_math.sh $model_path $output_dir $summary_path
+
+model_path="/gpfs/public/research/xy/yubowang/models/Qwen2.5-7B"
+output_dir="../math_eval_result_0110/qwen_eval_res_0110_new_math/ori_qwen2.5_7B/"
+bash eval_math.sh $model_path $output_dir $summary_path
+
+
+model_path="/gpfs/public/research/xy/yubowang/models/Qwen2.5-Math-7B"
+output_dir="../math_eval_result_0110/qwen_eval_res_0110_new_math/ori_qwen2.5_math_7B/"
+bash eval_math.sh $model_path $output_dir $summary_path
+
+
+model_path="/gpfs/public/research/xy/yubowang/models/Qwen2.5-Math-7B-Instruct"
+output_dir="../math_eval_result_0110/qwen_eval_res_0110_new_math/ori_qwen2.5_math_7B_instruct/"
+bash eval_math.sh $model_path $output_dir $summary_path
 
 
 model_sub_dir="qwen_math_output_models/CriticCoT_critic_data_1231/checkpoint-1171"
