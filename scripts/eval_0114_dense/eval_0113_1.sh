9011610809ac358e472e56396d005bdd4b6cbf21
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-14 04:32:33 +0800

    add

--- scripts/eval_0114_dense/eval_0113_1.sh
+++ scripts/eval_0114_dense/eval_0113_1.sh
@@ -6,7 +6,7 @@ conda activate lf_yubo
 
 export CUDA_VISIBLE_DEVICES=0
 # cd /gpfs/public/research/xy/yubowang/CriticCoT/Qwen2.5-Math-Eval/evaluation/sh
-
+cd /gpfs/public/research/xy/yubowang/CriticCoT/scripts/eval_0114_dense/
 
 summary_path="../math_eval_result_0114/ckpts_summary_0114_dense_p1.txt"
 root_dir="/gpfs/public/research/xy/yubowang/CriticCoT/LLaMA-Factory/output_models_dense_0112_qwen_math"
