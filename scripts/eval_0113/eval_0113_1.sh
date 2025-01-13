daec0628b5f970b253b3a52f52506864bb69bea9
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-13 21:42:19 +0800

    add

---
+++ scripts/eval_0113/eval_0113_1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -ex
+
+source /gpfs/public/research/miniconda3/bin/activate
+conda activate lf_yubo
+
+export CUDA_VISIBLE_DEVICES=1
+# cd /gpfs/public/research/xy/yubowang/CriticCoT/Qwen2.5-Math-Eval/evaluation/sh
+
+
+summary_path="../math_eval_result_0113/ckpts_summary_0113_dense.txt"
+root_dir="/gpfs/public/research/xy/yubowang/CriticCoT/LLaMA-Factory/output_models_dense_0112_qwen_math"
+
+find "$root_dir" -type d -name "qwen2.5*" | while read -r model_dir; do
+  echo $model_dir
+  bash eval_dir_models_others.sh $model_dir $summary_path
+done
