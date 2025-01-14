44843d37ebfe61aee79ef68c814d93b47624f8c9
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-14 14:35:40 +0800

    add

--- scripts/eval_0114_dense/eval_0113_2.sh
+++ scripts/eval_0114_dense/eval_0113_2.sh
@@ -8,7 +8,7 @@ export CUDA_VISIBLE_DEVICES=0
 # cd /gpfs/public/research/xy/yubowang/CriticCoT/Qwen2.5-Math-Eval/evaluation/sh
 cd /gpfs/public/research/xy/yubowang/CriticCoT/scripts/eval_0114_dense/
 
-summary_path="../math_eval_result_0114/ckpts_summary_0114_dense_p2.txt"
+summary_path="../math_eval_result_0114/ckpts_summary_0114_dense_p2_new.txt"
 root_dir="/gpfs/public/research/xy/yubowang/CriticCoT/LLaMA-Factory/output_models_dense_0112"
 
 find "$root_dir" -type d -name "qwen2.5-7B_critic_1231-0111" | while read -r model_dir; do
--- scripts/eval_0114_dense/eval_0113_3.sh
+++ scripts/eval_0114_dense/eval_0113_3.sh
@@ -8,7 +8,7 @@ export CUDA_VISIBLE_DEVICES=0
 cd /gpfs/public/research/xy/yubowang/CriticCoT/scripts/eval_0114_dense/
 
 
-summary_path="../math_eval_result_0114/ckpts_summary_0114_dense_p3.txt"
+summary_path="../math_eval_result_0114/ckpts_summary_0114_dense_p3_new.txt"
 root_dir="/gpfs/public/research/xy/yubowang/CriticCoT/LLaMA-Factory/output_models_dense_0112_qwen_math"
 
 find "$root_dir" -type d -name "qwen2.5-math-7B_critic_1231-0111" | while read -r model_dir; do
