cf9ddfa7b9cf0b9f3341474bd536e9f0590f5400
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-16 04:34:39 +0800

    add

---
+++ critique_on_math/critique_vllm.py
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+def get_prompt(question, solution):
+    input_text =
+    prompt = f"<|im_start|>system\nPlease reason step by step, and put your final answer within" \
+             f" \\boxed{{}}.<|im_end|>\n<|im_start|>user\n{input_text}<|im_end|>\n<|im_start|>assistant\n"
+
+
+def main():
+    return
+
+if __name__ == '__main__':
+    main()
+
