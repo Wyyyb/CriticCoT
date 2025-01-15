e8b05d122e0a36f4d84482a5fce1f50545412ef6
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-16 00:22:17 +0800

    add

---
+++ scripts/collect_math_train_critic_data_0115/process_critic_math_train.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -ex
+
+source /gpfs/public/research/miniconda3/bin/activate
+conda activate lf_yubo
+
+cd ../../Qwen2.5-Math-Eval/
+
+input_dir_base="math_eval_result_0115_critic_train/qwen_eval_res_0115_critic_train/"
+summary_path="math_eval_result_0115_critic_train/sta_critic_summary_0115.txt"
+
+# 遍历所有qwen2.5开头的子文件夹
+for input_dir in ${input_dir_base}/qwen2.5*/; do
+    if [ -d "$input_dir" ]; then
+        echo "Processing directory: $input_dir"
+        python postprocess_critic_result.py \
+            --input_dir "$input_dir" \
+            --summary_path "$summary_path" \
+            --candidate_num 8
+    fi
+done
