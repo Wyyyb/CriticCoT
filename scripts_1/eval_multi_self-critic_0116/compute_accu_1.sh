70e50fe7d9b19ad6111e1af7a57cfdbb4022f164
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-17 01:04:27 +0800

    add

--- scripts_1/eval_multi_self-critic_0116/compute_accu_1.sh
+++ scripts_1/eval_multi_self-critic_0116/compute_accu_1.sh
@@ -8,7 +8,7 @@ conda activate lf_yubo
 cd /gpfs/public/research/xy/yubowang/CriticCoT/critique_on_math
 
 input_dir="/gpfs/public/research/xy/yubowang/CriticCoT/Qwen2.5-Math-Eval/math_eval_result_self-critic_multi_0116/qwen_eval_res_0116_multi_result/qwen2.5-math-7B_multi_self-critic-0116-checkpoint-80/math/"
-summary_path="/gpfs/public/research/xy/yubowang/CriticCoT/Qwen2.5-Math-Eval/math_eval_result_MAmmoTH-Critique-1_ckpt-80_0116/qwen_eval_res_0116_multi_result/qwen2.5-math-7B_multi_self-critic-0116-checkpoint-80/math/summary.txt"
+summary_path="/gpfs/public/research/xy/yubowang/CriticCoT/Qwen2.5-Math-Eval/math_eval_result_self-critic_multi_0116/qwen_eval_res_0116_multi_result/qwen2.5-math-7B_multi_self-critic-0116-checkpoint-80/math/summary.txt"
 
 python calculate_multi_critic_accuracy.py \
     --input_dir $input_dir \
