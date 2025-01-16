6248910b479463e7bce5a268d03fe64d93e60449
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-16 20:14:55 +0800

    add

--- scripts/eval_0116_dense/deepseek_ori_eval.sh
+++ scripts/eval_0116_dense/deepseek_ori_eval.sh
@@ -4,11 +4,11 @@ set -ex
 source /gpfs/public/research/miniconda3/bin/activate
 conda activate lf_yubo
 
-export CUDA_VISIBLE_DEVICES=0,1,2,3
+export CUDA_VISIBLE_DEVICES=0
 cd /gpfs/public/research/xy/yubowang/CriticCoT/Qwen2.5-Math-Eval/evaluation/sh
 
-checkpoint_dir=""
-output_dir=""
-summary_path=""
+checkpoint_dir="/gpfs/public/research/xy/yubowang/models/deepseek-math-7b-base"
+output_dir="/gpfs/public/research/xy/yubowang/CriticCoT/Qwen2.5-Math-Eval/math_eval_result_0116_ori_deepseek"
+summary_path="/gpfs/public/research/xy/yubowang/CriticCoT/Qwen2.5-Math-Eval/math_eval_result_0116_deepseek/ori_deepseek_ckpts_summary_0116.txt"
 
 bash eval_deepseek_full.sh "$checkpoint_dir" "$output_dir" "$summary_path"
--- scripts/eval_0116_dense/mathstral_ori_eval.sh
+++ scripts/eval_0116_dense/mathstral_ori_eval.sh
@@ -4,11 +4,11 @@ set -ex
 source /gpfs/public/research/miniconda3/bin/activate
 conda activate lf_yubo
 
-export CUDA_VISIBLE_DEVICES=0,1,2,3
+export CUDA_VISIBLE_DEVICES=1
 cd /gpfs/public/research/xy/yubowang/CriticCoT/Qwen2.5-Math-Eval/evaluation/sh
 
-checkpoint_dir=""
-output_dir=""
-summary_path=""
+checkpoint_dir="/gpfs/public/research/xy/yubowang/models/Mathstral-7B-v0.1"
+output_dir="/gpfs/public/research/xy/yubowang/CriticCoT/Qwen2.5-Math-Eval/math_eval_result_0116_ori_Mathstral"
+summary_path="/gpfs/public/research/xy/yubowang/CriticCoT/Qwen2.5-Math-Eval/math_eval_result_0116_Mathstral/ori_Mathstral_ckpts_summary_0116.txt"
 
 bash eval_mathtral_full.sh "$checkpoint_dir" "$output_dir" "$summary_path"
