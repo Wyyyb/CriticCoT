5f1216d46136c58188c6201f36e838264c5775e6
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-17 00:50:28 +0800

    add

--- scripts_1/eval_deepseek_0117/deepseek_ori_eval.sh
+++ scripts_1/eval_deepseek_0117/deepseek_ori_eval.sh
@@ -4,7 +4,7 @@ set -ex
 source /gpfs/public/research/miniconda3/bin/activate
 conda activate lf_yubo
 
-export CUDA_VISIBLE_DEVICES=0,1
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 cd /gpfs/public/research/xy/yubowang/CriticCoT/Qwen2.5-Math-Eval/evaluation/sh
 
 checkpoint_dir="/gpfs/public/research/xy/yubowang/models/deepseek-math-7b-base"
