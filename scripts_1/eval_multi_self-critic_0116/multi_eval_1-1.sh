cba772f40736abeea722f01ecedde83b14e81f5b
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-16 23:59:31 +0800

    add

--- scripts_1/eval_multi_self-critic_0116/multi_eval_1-1.sh
+++ scripts_1/eval_multi_self-critic_0116/multi_eval_1-1.sh
@@ -13,7 +13,7 @@ cd /gpfs/public/research/xy/yubowang/CriticCoT/critique_on_math/
 
 ckpt="checkpoint-80"
 model_path="/gpfs/public/research/xy/yubowang/CriticCoT/LLaMA-Factory/output_models_0114/qwen2.5-math-7B_t2_critic_0114/${ckpt}"
-BASE_DIR="/gpfs/public/research/xy/yubowang/CriticCoT/Qwen2.5-Math-Eval/math_eval_result_self-critic_multi_0116"
+BASE_DIR="/gpfs/public/research/xy/yubowang/CriticCoT/Qwen2.5-Math-Eval/math_eval_result_MAmmoTH-Critique-1_ckpt-80_0116"
 input_dir="${BASE_DIR}/qwen_eval_res_0116_multi_result/MAmmoTH-Critique-1-0116-${ckpt}/"
 
 python run_multi_critique_on_vllm.py \
