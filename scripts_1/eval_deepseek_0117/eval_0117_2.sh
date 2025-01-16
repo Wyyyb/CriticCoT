c7e62122e072e7bda2d49f8af6f54cb92d43efa0
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-17 04:18:22 +0800

    add

---
+++ scripts_1/eval_deepseek_0117/eval_0117_2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -ex
+
+source /gpfs/public/research/miniconda3/bin/activate
+conda activate lf_yubo
+
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+# cd /gpfs/public/research/xy/yubowang/CriticCoT/Qwen2.5-Math-Eval/evaluation/sh
+cd /gpfs/public/research/xy/yubowang/CriticCoT/scripts_1/eval_deepseek_0117/
+
+summary_path="/gpfs/public/research/xy/yubowang/CriticCoT/math_eval_result_0117_dense_Mathstral_math_eval/Mathstral_ckpts_summary_0117_p0.txt"
+root_dir="/gpfs/public/research/xy/yubowang/CriticCoT/LLaMA-Factory/Mathstral_math_output_models"
+
+find "$root_dir" -type d -name "CriticCoT_correct_only_data_0114" | while read -r model_dir; do
+  echo $model_dir
+  bash eval_dir_deepseek_models_math.sh $model_dir $summary_path
+done
