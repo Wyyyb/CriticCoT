e4ed57309a88a40d2ac15a5869f6caf0ce6f872d
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-17 02:46:34 +0800

    add

---
+++ scripts_1/eval_MAmmoTH-Critique-2_0117_dense/eval_0117_1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -ex
+
+source /gpfs/public/research/miniconda3/bin/activate
+conda activate lf_yubo
+
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+# cd /gpfs/public/research/xy/yubowang/CriticCoT/Qwen2.5-Math-Eval/evaluation/sh
+cd /gpfs/public/research/xy/yubowang/CriticCoT/scripts_1/eval_MAmmoTH-Critique-2_0117_dense/
+
+summary_path="../math_eval_result_MAmmoTH-Critique-2_0117_dense/summary_0117.txt"
+root_dir="/gpfs/public/research/xy/yubowang/CriticCoT/LLaMA-Factory/output_models_0117"
+
+find "$root_dir" -type d -name "qwen2.5-math-7B_self_critique_ckpt-380_MATH-TRAIN-8_data_0116" | while read -r model_dir; do
+  echo $model_dir
+  bash eval_dir_models_math.sh $model_dir $summary_path
+done
