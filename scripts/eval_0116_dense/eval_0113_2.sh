fed3518e321b68e0809a578cf8de1cef3a934fb0
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-16 20:11:12 +0800

    add

--- scripts/eval_0116_dense/eval_0113_2.sh
+++ scripts/eval_0116_dense/eval_0113_2.sh
@@ -6,7 +6,7 @@ conda activate lf_yubo
 
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 # cd /gpfs/public/research/xy/yubowang/CriticCoT/Qwen2.5-Math-Eval/evaluation/sh
-cd /gpfs/public/research/xy/yubowang/CriticCoT/scripts/eval_0114_dense/
+cd /gpfs/public/research/xy/yubowang/CriticCoT/scripts/eval_0116_dense/
 
 summary_path="/gpfs/public/research/xy/yubowang/CriticCoT/Qwen2.5-Math-Eval/math_eval_result_0116_Mathstral/Mathstral_ckpts_summary_0116_dense_p0_new.txt"
 root_dir="/gpfs/public/research/xy/yubowang/CriticCoT/LLaMA-Factory/Mathstral_math_output_models"
