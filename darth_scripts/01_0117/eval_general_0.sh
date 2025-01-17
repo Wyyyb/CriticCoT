87cb973f6d3299ca4461044a35bb169e559d933a
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-18 04:34:21 +0800

    add

--- darth_scripts/01_0117/eval_general_0.sh
+++ darth_scripts/01_0117/eval_general_0.sh
@@ -1,13 +1,14 @@
 set -ex
 
-#conda activate critic
+source /gpfs/public/research/miniconda3/bin/activate
+conda activate lf_yubo
 
-export CUDA_VISIBLE_DEVICES=6,7
-cd ../eval_scripts
+export CUDA_VISIBLE_DEVICES=0
+cd ../eval_scripts_01
 
-model_dir="/data/yubowang/critic_models"
+model_dir="/gpfs/public/research/xy/yubowang/models"
 #model_names=("Abel-7B-002" "deepseek-math-7b-base" "gemma-2-9b" "internlm2-math-7b" "Llama-3.1-8B" "Llama-3.1-8B-Instruct" "Llama-3-8B" "Qwen2.5-7B" "Qwen2.5-Math-7B" "Qwen2.5-Math-7B-Instruct" "Qwen2-7B" "Qwen2-Math-7B" "rho-math-7b-v0.1" "WizardMath-7B-V1.1")
-model_names=("gemma-2-9b" "Llama-3.1-8B" "Llama-3.1-8B-Instruct" "Qwen2-7B" "Qwen2-Math-7B" "Qwen2.5-7B" "Qwen2.5-Math-7B" "Qwen2.5-Math-7B-Instruct")
+model_names=("Qwen2.5-7B" "Qwen2.5-Math-7B" "Qwen2.5-Math-7B-Instruct")
 
 #output_dir="/data/yubowang/CriticCoT/eval_results_darth/general_results"
 #summary_dir="/data/yubowang/CriticCoT/eval_results_darth/general_summary"
@@ -18,8 +19,8 @@ model_names=("gemma-2-9b" "Llama-3.1-8B" "Llama-3.1-8B-Instruct" "Qwen2-7B" "Qwe
 
 for model in "${model_names[@]}";do
   model_path="${model_dir}/${model}"
-  output_dir="/data/yubowang/CriticCoT/eval_results_darth/general_results/${model}"
-  summary_dir="/data/yubowang/CriticCoT/eval_results_darth/general_results/${model}_general_summary.txt"
+  output_dir="/gpfs/public/research/xy/yubowang/CriticCoT/eval_results_darth/general_results/${model}"
+  summary_dir="/gpfs/public/research/xy/yubowang/CriticCoT/eval_results_darth/general_results/${model}_general_summary.txt"
   mkdir -p $output_dir
   bash eval_general_reasoning.sh $model_path $output_dir $summary_dir
 
--- darth_scripts/01_0117/eval_mmlu-pro_0.sh
+++ darth_scripts/01_0117/eval_mmlu-pro_0.sh
@@ -1,14 +1,15 @@
 set -ex
 
-#conda activate critic
+source /gpfs/public/research/miniconda3/bin/activate
+conda activate lf_yubo
 
-export CUDA_VISIBLE_DEVICES=4,5
-cd ../eval_scripts
+export CUDA_VISIBLE_DEVICES=1
+cd ../eval_scripts_01
 
-model_dir="/data/yubowang/critic_models"
+model_dir="/gpfs/public/research/xy/yubowang/models"
 #model_names=("Abel-7B-002" "deepseek-math-7b-base" "gemma-2-9b" "internlm2-math-7b" "Llama-3.1-8B" "Llama-3.1-8B-Instruct" "Llama-3-8B" "Qwen2.5-7B" "Qwen2.5-Math-7B" "Qwen2.5-Math-7B-Instruct" "Qwen2-7B" "Qwen2-Math-7B" "rho-math-7b-v0.1" "WizardMath-7B-V1.1")
 #model_names=("deepseek-math-7b-base" "internlm2-math-7b" "Llama-3.1-8B" "Llama-3.1-8B-Instruct" "Qwen2.5-7B" "Qwen2.5-Math-7B" "Qwen2.5-Math-7B-Instruct" "Qwen2-Math-7B" "rho-math-7b-v0.1" "WizardMath-7B-V1.1")
-model_names=("deepseek-math-7b-base" "internlm2-math-7b" "Llama-3.1-8B" "Llama-3.1-8B-Instruct" "Qwen2-Math-7B" "rho-math-7b-v0.1" "WizardMath-7B-V1.1" "Qwen2.5-7B" "Qwen2.5-Math-7B" "Qwen2.5-Math-7B-Instruct")
+model_names=("Qwen2.5-7B" "Qwen2.5-Math-7B" "Qwen2.5-Math-7B-Instruct")
 
 #for model in "${model_names}"; do
 #  echo ${model}
@@ -16,8 +17,8 @@ model_names=("deepseek-math-7b-base" "internlm2-math-7b" "Llama-3.1-8B" "Llama-3
 
 for model in "${model_names[@]}"; do
   model_path="${model_dir}/${model}"
-  output_dir="/data/yubowang/CriticCoT/eval_results_darth/mmlu-pro_results/${model}"
-  summary_dir="/data/yubowang/CriticCoT/eval_results_darth/${model}_mmlu-pro_summary"
+  output_dir="/gpfs/public/research/xy/yubowang/CriticCoT/eval_results_darth/mmlu-pro_results/${model}"
+  summary_dir="/gpfs/public/research/xy/yubowang/CriticCoT/eval_results_darth/${model}_mmlu-pro_summary"
   bash eval_mmlu_pro.sh $model_path $output_dir $summary_dir
 
 done
