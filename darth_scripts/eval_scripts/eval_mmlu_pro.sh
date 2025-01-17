be3b4c54138317db7b8a2d3d2f75da15245739c3
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-18 01:55:08 +0800

    add

--- darth_scripts/eval_scripts/eval_mmlu_pro.sh
+++ darth_scripts/eval_scripts/eval_mmlu_pro.sh
@@ -5,6 +5,6 @@ output_dir=$2
 summary_path=$3
 # n_shot=$4
 
-cd /gpfs/public/research/xy/yubowang/CriticCoT/MMLU-Pro
+cd /data/yubowang/CriticCoT/MMLU-Pro
 bash mmlu-pro-eval.sh $model_path $output_dir $summary_path
 
