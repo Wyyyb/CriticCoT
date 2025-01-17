f59278adbd5f6a396b34d93d8240cd2a693e59a2
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-18 03:42:36 +0800

    add

--- darth_scripts/eval_0117/eval_mmlu-pro_0.sh
+++ darth_scripts/eval_0117/eval_mmlu-pro_0.sh
@@ -7,7 +7,7 @@ cd ../eval_scripts
 
 model_dir="/data/yubowang/critic_models"
 #model_names=("Abel-7B-002" "deepseek-math-7b-base" "gemma-2-9b" "internlm2-math-7b" "Llama-3.1-8B" "Llama-3.1-8B-Instruct" "Llama-3-8B" "Qwen2.5-7B" "Qwen2.5-Math-7B" "Qwen2.5-Math-7B-Instruct" "Qwen2-7B" "Qwen2-Math-7B" "rho-math-7b-v0.1" "WizardMath-7B-V1.1")
-model_names=("Abel-7B-002" "deepseek-math-7b-base" "internlm2-math-7b" "Llama-3.1-8B" "Llama-3.1-8B-Instruct" "Qwen2.5-7B" "Qwen2.5-Math-7B" "Qwen2.5-Math-7B-Instruct" "Qwen2-Math-7B" "rho-math-7b-v0.1" "WizardMath-7B-V1.1")
+model_names=("deepseek-math-7b-base" "internlm2-math-7b" "Llama-3.1-8B" "Llama-3.1-8B-Instruct" "Qwen2.5-7B" "Qwen2.5-Math-7B" "Qwen2.5-Math-7B-Instruct" "Qwen2-Math-7B" "rho-math-7b-v0.1" "WizardMath-7B-V1.1")
 
 #for model in "${model_names}"; do
 #  echo ${model}
