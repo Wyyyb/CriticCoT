ae65c37953da92ec37a20afd145e310ece662239
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-19 01:51:42 +0800

    add

---
+++ preprocess_webinstruct_data/test_trans_parquet.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import json
+
+# 读取parquet文件
+df = pd.read_parquet('math_sft.parquet')
+
+# 转换为JSON文件
+output_file = '../local_data/AceMath-Instruct-Training-Data/math_sft.jsonl'
+with open(output_file, 'w', encoding='utf-8') as f:
+    # 逐行写入JSONL格式
+    for _, row in df.iterrows():
+        json_str = json.dumps(row.to_dict(), ensure_ascii=False)
+        f.write(json_str + '\n')
