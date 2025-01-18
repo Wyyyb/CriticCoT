0ddd07895d3a115f0ed43b2a80532be70f488a83
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-18 15:00:45 +0800

    add

--- u_utils/hf_transfer.sh
+++ u_utils/hf_transfer.sh
@@ -16,9 +16,18 @@ huggingface-cli download meta-math/MetaMathQA --local-dir . --repo-type dataset
 
 huggingface-cli upload ubowang/critic_training_data_0111 . --repo-type dataset
 
+huggingface-cli upload ubowang/CFT_data_0118 . --repo-type dataset
+
+huggingface-cli download ubowang/CFT_data_0118 . --repo-type dataset
+
 huggingface-cli upload ubowang/MAmmoTH-Critique-1 . --repo-type model
 
 pip install modelscope
 
 modelscope upload ubowang/MAmmoTH-Critique-1 . --repo-type model
 
+modelscope upload ubowang/CFT_data_0118 . --repo-type dataset
+
+modelscope download ubowang/CFT_data_0118 . --repo-type dataset
+
+modelscope download --dataset 'ubowang/CFT_data_0118' --local_dir '.'
