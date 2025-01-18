f5d486c154a7c078c7b5204f82165c1b7d467db3
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-18 16:21:42 +0800

    add

--- scripts_0118/eval_baseline_0118/eval_general_0.sh
+++ scripts_0118/eval_baseline_0118/eval_general_0.sh
@@ -7,16 +7,9 @@ export CUDA_VISIBLE_DEVICES=0,1,2,3
 cd /gpfs/public/research/xy/yubowang/CriticCoT/scripts_0118/eval_baseline_0118
 
 model_dir="/gpfs/public/research/xy/yubowang/models"
-#model_names=("Abel-7B-002" "deepseek-math-7b-base" "gemma-2-9b" "internlm2-math-7b" "Llama-3.1-8B" "Llama-3.1-8B-Instruct" "Llama-3-8B" "Qwen2.5-7B" "Qwen2.5-Math-7B" "Qwen2.5-Math-7B-Instruct" "Qwen2-7B" "Qwen2-Math-7B" "rho-math-7b-v0.1" "WizardMath-7B-V1.1")
-# model_names=("Qwen2.5-7B" "Qwen2.5-Math-7B" "Qwen2.5-Math-7B-Instruct")
+
 model_names=("MAmmoTH-Critique-1")
 
-#output_dir="/data/yubowang/CriticCoT/eval_results_darth/general_results"
-#summary_dir="/data/yubowang/CriticCoT/eval_results_darth/general_summary"
-
-#for model in "${model_names[@]}";do
-#  echo "${model}"
-#done
 
 for model in "${model_names[@]}";do
   model_path="${model_dir}/${model}"
--- scripts_0118/eval_baseline_0118/eval_math_hard.sh
+++ scripts_0118/eval_baseline_0118/eval_math_hard.sh
@@ -8,13 +8,14 @@ export CUDA_VISIBLE_DEVICES=0,1,2,3
 cd /gpfs/public/research/xy/yubowang/CriticCoT/scripts_0118/eval_baseline_0118
 model_dir="/gpfs/public/research/xy/yubowang/models"
 
-model_names=("MAmmoTH-Critique-1" "AceMath-7B-Instruct" "Eurus-2-7B-SFT" "NuminaMath-72B-CoT" "Qwen2.5-Math-72B-Instruct")
+model_names=("MAmmoTH-Critique-1" "AceMath-7B-Instruct" "deepseek-math-7b-instruct" "Llama-3.1-8B-Instruct" "NuminaMath-72B-CoT" "Qwen2.5-Math-72B-Instruct" "Llama-3.1-70B-Instruct")
 
 
 for model in "${model_names[@]}"; do
   model_path="${model_dir}/${model}"
   output_dir="/gpfs/public/research/xy/yubowang/CriticCoT/eval_results_baseline_0118/math_hard_results/${model}"
   summary_dir="/gpfs/public/research/xy/yubowang/CriticCoT/eval_results_baseline_0118/${model}_math_hard_summary.txt"
+  mkdir -p $output_dir
   bash run_math_eval.sh $model_path $output_dir $summary_dir
 
 done
