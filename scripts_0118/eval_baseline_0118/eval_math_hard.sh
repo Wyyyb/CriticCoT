7c8c0b372b1acc8502e3575ce818e8b30732bd27
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-18 17:21:40 +0800

    add

--- scripts_0118/eval_baseline_0118/eval_math_hard.sh
+++ scripts_0118/eval_baseline_0118/eval_math_hard.sh
@@ -3,13 +3,14 @@ set -ex
 source /gpfs/public/research/miniconda3/bin/activate
 conda activate lf_yubo
 
-export CUDA_VISIBLE_DEVICES=0,1,2,3
+export CUDA_VISIBLE_DEVICES=0,1
 
 cd /gpfs/public/research/xy/yubowang/CriticCoT/scripts_0118/eval_baseline_0118
 model_dir="/gpfs/public/research/xy/yubowang/models"
 
-model_names=("MAmmoTH-Critique-1" "AceMath-7B-Instruct" "deepseek-math-7b-instruct" "Llama-3.1-8B-Instruct" "NuminaMath-72B-CoT" "Qwen2.5-Math-72B-Instruct" "Llama-3.1-70B-Instruct")
+#model_names=("MAmmoTH-Critique-1" "AceMath-7B-Instruct" "deepseek-math-7b-instruct" "Llama-3.1-8B-Instruct" "NuminaMath-72B-CoT" "Qwen2.5-Math-72B-Instruct" "Llama-3.1-70B-Instruct")
 
+model_names=("deepseek-math-7b-instruct" "Llama-3.1-8B-Instruct" "Llama-3.1-70B-Instruct")
 
 for model in "${model_names[@]}"; do
   model_path="${model_dir}/${model}"
