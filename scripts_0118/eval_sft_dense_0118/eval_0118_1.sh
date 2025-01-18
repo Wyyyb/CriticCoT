074126a6d3dea0d0e9e4fbf58a9c5b0da57ef562
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-18 15:24:17 +0800

    add

---
+++ scripts_0118/eval_sft_dense_0118/eval_0118_1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -ex
+
+source /gpfs/public/research/miniconda3/bin/activate
+conda activate lf_yubo
+
+export CUDA_VISIBLE_DEVICES=1
+# cd /gpfs/public/research/xy/yubowang/CriticCoT/Qwen2.5-Math-Eval/evaluation/sh
+cd /gpfs/public/research/xy/yubowang/CriticCoT/scripts_0118/eval_sft_dense_0118/
+
+summary_path="/gpfs/public/research/xy/yubowang/CriticCoT/math_eval_result_0118/numina_summary_0118_dense.txt"
+root_dir="/gpfs/public/research/xy/yubowang/CriticCoT/LLaMA-Factory/output_models_0118"
+
+find "$root_dir" -type d -name "qwen2.5-math-7B_numina_sample_80k_data_0118" | while read -r model_dir; do
+  echo $model_dir
+  bash eval_dir_models_math.sh $model_dir $summary_path
+done
