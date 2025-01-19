a462b5183d3d6da9df458e00b5b222f07028ee9c
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-19 22:49:12 +0800

    add

--- vllm_inference/run_vllm_webinstruct.sh
+++ vllm_inference/run_vllm_webinstruct.sh
@@ -7,7 +7,8 @@ conda activate lf_yubo
 cd /gpfs/public/research/xy/yubowang/CriticCoT/vllm_inference
 
 # 设置CUDA设备
-export CUDA_VISIBLE_DEVICES=0,1,2,3
+#export CUDA_VISIBLE_DEVICES=0,1,2,3
+export CUDA_VISIBLE_DEVICES=0,1
 
 python qwen_math_run_webinstruct_80k_0119.py
 
