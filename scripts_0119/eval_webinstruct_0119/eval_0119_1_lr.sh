7b66d41425e99390e62ebd5a5853980f6a26fcb6
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-19 20:03:41 +0800

    add

---
+++ scripts_0119/eval_webinstruct_0119/eval_0119_1_lr.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -ex
+
+source /gpfs/public/research/miniconda3/bin/activate
+conda activate lf_yubo
+
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+# cd /gpfs/public/research/xy/yubowang/CriticCoT/Qwen2.5-Math-Eval/evaluation/sh
+cd /gpfs/public/research/xy/yubowang/CriticCoT/scripts_0119/eval_webinstruct_0119/
+
+summary_path="/gpfs/public/research/xy/yubowang/CriticCoT/math_eval_result_qwen2.5-math-7B_webinstruct_sft_gpt4o_80k_0119_lr/summary.txt"
+root_dir="/gpfs/public/research/xy/yubowang/CriticCoT/LLaMA-Factory/output_models_0119"
+
+find "$root_dir" -type d -name "qwen2.5-math-7B_webinstruct_sft_gpt4o_80k_0119_lr" | while read -r model_dir; do
+  echo $model_dir
+  bash eval_dir_models_math.sh $model_dir $summary_path
+done
