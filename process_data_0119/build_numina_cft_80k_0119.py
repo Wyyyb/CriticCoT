33a18034f98b417136f28e4db827d96db3b4e182
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-19 20:28:15 +0800

    add

--- process_data_0119/build_numina_cft_80k_0119.py
+++ process_data_0119/build_numina_cft_80k_0119.py
@@ -1,7 +1,46 @@
 import json
+import os
 
 
+def main():
+    input_dir = ""
+    cft_output_file_path = "../LLaMA-Factory/data/numina_cft_80k_0119.json"
+    sft_output_file_path = "../LLaMA-Factory/data/numina_sft_80k_0119.json"
+    cft_data = []
+    sft_data = []
+    for each in os.listdir(input_dir):
+        if not each.endswith(".json"):
+            continue
+        file_path = os.path.join(input_dir, each)
+        with open(file_path, "r") as fi:
+            curr = json.load(fi)
+        for item in curr:
+            question = item["question"]
+            numina_solution = item["numina_solution"]
+            answer = item["qwen_2.5_math_answer"]
+            if "model_output" not in item or item["model_output"] == "":
+                print("model output empty", item)
+                continue
+            critique = item["model_output"]
+            cft_data.append(single_format_cft(question, answer, critique))
+            sft_data.append(single_format_sft(question, numina_solution))
+    with open(cft_output_file_path, "w") as fo:
+        fo.write(json.dumps(cft_data, indent=4))
+
+    with open(sft_output_file_path, "w") as fo:
+        fo.write(json.dumps(sft_data, indent=4))
 
 
+def single_format_cft(question, solution, critique):
+    t2_curr = {"instruction": "Please critique whether the following solution to the question is correct.\n\n",
+               "input": question + f"\nSolution:\n{solution}\n", "output": critique}
+    return t2_curr
 
+
+def single_format_sft(question, answer):
+    return {"instruction": "Please analyze and answer the following question step by step:",
+            "input": question, "output": answer}
+
+
+main()
 
---
+++ scripts_0119/qwen_math_numina_sft_0119/train_0.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+source /gpfs/public/research/miniconda3/bin/activate
+conda activate lf_yubo
+
+# cd /data/yubo/CriticCoT/LLaMA-Factory/
+cd /gpfs/public/research/xy/yubowang/CriticCoT/LLaMA-Factory/
+PROJECT_NAME="critic_cot"
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+export WANDB_API_KEY=f318ffd0dcf5d31701fd33aee12e57e9cf15444f
+export WANDB_PROJECT=$PROJECT_NAME
+export WANDB_MODE=disabled
+MASTER_PORT=$(shuf -i 20000-30000 -n 1)
+export MASTER_PORT
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+
+MODEL_NAME="critic-proj"
+export WANDB_RUN_NAME=$MODEL_NAME
+
+FORCE_TORCHRUN=1 llamafactory-cli train ../scripts_0119/qwen_math_numina_sft_0119/qwen2.5-math-7B_numina_sft_80k_0119.yaml
+
