e932e0ac3234700e739b0e0a76d4cdf308680af0
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-21 05:07:46 +0800

    add

--- process_data_ali_0120/build_acemath_gpt4o_cft_10k_0121.py
+++ process_data_ali_0120/build_acemath_gpt4o_cft_10k_0121.py
@@ -1,28 +1,39 @@
 import json
 import os
+import random
 
 
 def main():
-    input_file_path = "/cpfs/data/user/yubowang/CriticCoT/local_data/data_0120/qwen_math_ace_80k_0119_ace_critique.json"
-    cft_output_file_path = "../LLaMA-Factory/data/ace_80k_critique_ace_0120.json"
-    sft_output_file_path = "../LLaMA-Factory/data/ace_80k_sft_0120.json"
+    input_dir = "/cpfs/data/user/yubowang/CriticCoT/local_data/ace_10k_cft_0121"
+    cft_output_file_path = "../LLaMA-Factory/data/ace_10k_gpt4o_cft_0121.json"
+    cft_1_output_file_path = "../LLaMA-Factory/data/webinstruct_ace_80k_gpt4o_cft_0121.json"
     cft_data = []
     sft_data = []
-
-    with open(input_file_path, "r") as fi:
-        input_data = json.load(fi)
-    for item in input_data:
-        question = item["question"]
-        ace_math_solution = item["ace_math_solution"]
-        critique = item["ace_gemini_critique"]
-        cft_data.append(single_format_cft(question, ace_math_solution, critique))
-        sft_data.append(single_format_sft(question, ace_math_solution))
+    for each in os.listdir(input_dir):
+        if not each.endswith(".json"):
+            continue
+        file_path = os.path.join(input_dir, each)
+        with open(file_path, "r") as fi:
+            curr = json.load(fi)
+        for item in curr:
+            question = "Question:\n" + item["input"]
+            solution = item["output"]
+            if "model_output" not in item or item["model_output"] == "":
+                print("model output empty", item)
+                continue
+            critique = item["model_output"]
+            cft_data.append(single_format_cft(question, solution, critique))
     print("len(cft_data)", len(cft_data))
-    print("len(sft_data)", len(sft_data))
     with open(cft_output_file_path, "w") as fo:
         fo.write(json.dumps(cft_data, indent=4))
-    with open(sft_output_file_path, "w") as fo:
-        fo.write(json.dumps(sft_data, indent=4))
+    wi_data_path = "/cpfs/data/user/yubowang/CriticCoT/LLaMA-Factory/data/webinstruct_cft_80k_0119.json"
+    with open(wi_data_path, "r", encoding="utf-8") as fi:
+        wi_data = json.load(fi)
+    cft_1_data = wi_data[:70000] + cft_data
+    random.shuffle(cft_1_data)
+    print("len(cft_1_data)", len(cft_1_data))
+    with open(cft_1_output_file_path, "w") as fo:
+        fo.write(json.dumps(cft_1_data, indent=4))
 
 
 def single_format_cft(question, solution, critique):
