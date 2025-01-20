b5688969a6caabd4da2a8a21adf6cba34058daf5
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-20 15:27:28 +0800

    add

--- process_data_ali_0120/build_acemath_on-policy_cft_80k_0120.py
+++ process_data_ali_0120/build_acemath_on-policy_cft_80k_0120.py
@@ -3,26 +3,21 @@ import os
 
 
 def main():
-    input_file_path = "/cpfs/data/user/yubowang/CriticCoT/local_data/data_0120/qwen_math_ace_80k_0119_ace_critique.json"
-    cft_output_file_path = "../LLaMA-Factory/data/ace_80k_critique_ace_0120.json"
-    sft_output_file_path = "../LLaMA-Factory/data/ace_80k_sft_0120.json"
+    input_file_path = "/cpfs/data/user/yubowang/CriticCoT/local_data/data_0120/" \
+                      "qwen_math_ace_80k_0119_qwen_critique.json"
+    cft_output_file_path = "../LLaMA-Factory/data/ace_80k_critique_qwen_on-policy_0120.json"
     cft_data = []
-    sft_data = []
 
     with open(input_file_path, "r") as fi:
         input_data = json.load(fi)
     for item in input_data:
         question = item["question"]
-        ace_math_solution = item["ace_math_solution"]
-        critique = item["ace_gemini_critique"]
-        cft_data.append(single_format_cft(question, ace_math_solution, critique))
-        sft_data.append(single_format_sft(question, ace_math_solution))
+        qwen_2_5_math_answer = item["qwen_2.5_math_answer"]
+        critique = item["qwen_gemini_critique"]
+        cft_data.append(single_format_cft(question, qwen_2_5_math_answer, critique))
     print("len(cft_data)", len(cft_data))
-    print("len(sft_data)", len(sft_data))
     with open(cft_output_file_path, "w") as fo:
         fo.write(json.dumps(cft_data, indent=4))
-    with open(sft_output_file_path, "w") as fo:
-        fo.write(json.dumps(sft_data, indent=4))
 
 
 def single_format_cft(question, solution, critique):
