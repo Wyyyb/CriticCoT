c2f8face9941634ac75581c88ed136e0b4b6c458
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-21 03:10:44 +0800

    add

---
+++ process_data_ali_0120/sample_webinstruct_0121.py
@@ -0,0 +1,40 @@
+import json
+import os
+
+
+def main():
+    input_file = "../LLaMA-Factory/data/CriticCoT_t2_critic_data_0115.json"
+    output_file_base_path = "../LLaMA-Factory/data/webinstruct_cft_80k_0121_p"
+    with open(input_file, "r", encoding="utf-8") as fi:
+        ori_data = json.load(fi)
+    input_length_data = sorted(ori_data, key=lambda x: len(x["input"]))
+    output_length_data = sorted(ori_data, key=lambda x: len(x["output"]))
+    total_length_data = sorted(ori_data, key=lambda x: len(x["input"]) + len(x["output"]))
+    data_number = 80000
+    edge_window = 1000
+    output_data = []
+    # output_data[0]: input length最长的80k
+    # output_data[1]: input length最短的80k
+    # output_data[2]: output length最长的80k
+    # output_data[3]: output length最短的80k
+    # output_data[4]: input + output length平均的80k
+    input_length_data = input_length_data[edge_window: -edge_window]
+    output_length_data = output_length_data[edge_window: -edge_window]
+    output_data.append(input_length_data[-1 * data_number:])
+    output_data.append(input_length_data[:data_number])
+
+    output_data.append(output_length_data[-1 * data_number:])
+    output_data.append(output_length_data[:data_number])
+
+    output_data.append(total_length_data[260000:340000])
+
+    for i in range(len(output_data)):
+        output_path = output_file_base_path + str(i + 1) + ".json"
+        print("len(output_data[i])", len(output_data[i]))
+        with open(output_path, "w") as fo:
+            fo.write(json.dumps(output_data[i], indent=4))
+
+
+main()
+
+
