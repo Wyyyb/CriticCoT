8263317a56ccb462d96c532f02a117cd0b31fd13
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-22 02:23:03 +0800

    add

--- process_data_ali_0120/add_ace_cft_80k_batch_api_0122.py
+++ process_data_ali_0120/add_ace_cft_80k_batch_api_0122.py
@@ -2,12 +2,11 @@ from openai import OpenAI
 client = OpenAI()
 
 batch_input_file = client.files.create(
-    file=open("/cpfs/data/user/yubowang/CriticCoT/local_data/batch_data_0122/batchinput.jsonl", "rb"),
+    file=open("/cpfs/data/user/yubowang/CriticCoT/local_data/batch_data_0122/batchinput-1.jsonl", "rb"),
     purpose="batch"
 )
 
 print(batch_input_file)
-
 batch_input_file_id = batch_input_file.id
 print("batch_input_file_id", batch_input_file_id)
 client.batches.create(
