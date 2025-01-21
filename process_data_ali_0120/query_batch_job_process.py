535ea252b9042f52c9b5dc026e08da4a03ad037a
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-22 02:33:43 +0800

    add

--- process_data_ali_0120/query_batch_job_process.py
+++ process_data_ali_0120/query_batch_job_process.py
@@ -1,8 +1,8 @@
 from openai import OpenAI
 client = OpenAI()
 
-batch = client.batches.retrieve("batch_4Qf5iq6tgTEjS4TjWdz2bo")
-print(batch)
-batch = client.batches.retrieve("batch_YBxjVob2c4juUpdGJeM4y7")
+# batch = client.batches.retrieve("batch_4Qf5iq6tgTEjS4TjWdz2bo")
+# print(batch)
+batch = client.batches.retrieve("YBxjVob2c4juUpdGJeM4y7")
 print(batch)
 
