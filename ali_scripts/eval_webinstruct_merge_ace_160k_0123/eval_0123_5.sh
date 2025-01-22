f33cdde1dffa9161bc947d9337939acd2bc098fe
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-23 00:17:57 +0800

    add

---
+++ ali_scripts/eval_webinstruct_merge_ace_160k_0123/eval_0123_5.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -ex
+
+source /cpfs/data/shared/public/miniconda3/bin/activate
+conda activate lf_yubo
+
+export CUDA_VISIBLE_DEVICES=4
+# cd /gpfs/public/research/xy/yubowang/CriticCoT/Qwen2.5-Math-Eval/evaluation/sh
+cd /cpfs/data/user/yubowang/CriticCoT/ali_scripts/eval_webinstruct_merge_ace_160k_0123/
+
+summary_path="/cpfs/data/user/yubowang/CriticCoT/math_eval_result_webinstruct_merge_ace_cft_160k_0123/summary.txt"
+root_dir="/cpfs/data/user/yubowang/CriticCoT/LLaMA-Factory/output_models_0123"
+
+find "$root_dir" -type d -name "webinstruct_merge_ace_cft_160k_0123" | while read -r model_dir; do
+  echo $model_dir
+  bash eval_dir_models_math_5.sh $model_dir $summary_path
+done
---
+++ ali_scripts/eval_webinstruct_merge_ace_160k_0123/eval_0123_start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -ex
+
+source /cpfs/data/shared/public/miniconda3/bin/activate
+conda activate lf_yubo
+
+bash eval_0123_1.sh &
+bash eval_0123_2.sh &
+bash eval_0123_3.sh &
+bash eval_0123_4.sh &
+bash eval_0123_5.sh &
+bash eval_0123_6.sh &
+bash eval_0123_7.sh &
+bash eval_0123_8.sh &
