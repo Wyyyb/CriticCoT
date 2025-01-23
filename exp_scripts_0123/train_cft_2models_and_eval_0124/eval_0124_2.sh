55a344e4c0deb529b4afd4fa9354e46cc2c07f1d
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-24 00:29:45 +0800

    add

---
+++ exp_scripts_0123/train_cft_2models_and_eval_0124/eval_0124_2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -ex
+
+source /cpfs/data/shared/public/miniconda3/bin/activate
+conda activate lf_yubo
+
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+# cd /gpfs/public/research/xy/yubowang/CriticCoT/Qwen2.5-Math-Eval/evaluation/sh
+cd /cpfs/data/user/yubowang/CriticCoT/exp_scripts_0123/train_cft_2models_and_eval_0124/
+
+summary_path="/cpfs/data/user/yubowang/CriticCoT/math_eval_result_deepseek-math-7b-base_webinstruct_cft_80k_0119/summary.txt"
+root_dir="/cpfs/data/user/yubowang/CriticCoT/LLaMA-Factory/output_models_0124"
+
+find "$root_dir" -type d -name "deepseek-math-7b-base_webinstruct_cft_80k_0119" | while read -r model_dir; do
+  echo $model_dir
+  bash eval_dir_models_math.sh $model_dir $summary_path
+done
