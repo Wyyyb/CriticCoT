e96d9b297fa02f0a3aaaac9737ef2044d27a124b
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-24 00:25:47 +0800

    add

--- process_data_ali_0122/add_metamath_cft_80k_batch_api_0123.py
+++ process_data_ali_0122/add_metamath_cft_80k_batch_api_0123.py
@@ -45,3 +45,11 @@ batch_input_file_id file-YBxjVob2c4juUpdGJeM4y7
 FileObject(id='file-4qr7yZLDgAZ1yz3Czm2t9f', bytes=217996954, created_at=1737482842, filename='batchinput.jsonl', object='file', purpose='batch', status='processed', status_details=None)
 '''
 
+'''
+0124:
+FileObject(id='file-TQ6HSqC4hFGmnHrbLkkCrB', bytes=51029744, created_at=1737648277, filename='batchinput-1.jsonl', object='file', purpose='batch', status='processed', status_details=None)
+batch_input_file_id file-TQ6HSqC4hFGmnHrbLkkCrB
+FileObject(id='file-AJJR4EcxVVqXVspQ9LrtcY', bytes=51066612, created_at=1737648300, filename='batchinput-2.jsonl', object='file', purpose='batch', status='processed', status_details=None)
+batch_input_file_id file-AJJR4EcxVVqXVspQ9LrtcY
+'''
+
