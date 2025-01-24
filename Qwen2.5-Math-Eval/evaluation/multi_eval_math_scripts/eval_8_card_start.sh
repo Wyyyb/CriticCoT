2fa63a4cc22fc77b75028774493c419d19c5e267
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-24 18:14:15 +0800

    add

--- Qwen2.5-Math-Eval/evaluation/multi_eval_math_scripts/eval_8_card_start.sh
+++ Qwen2.5-Math-Eval/evaluation/multi_eval_math_scripts/eval_8_card_start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -ex
 
-source /cpfs/data/shared/public/miniconda3/bin/activate
-conda activate lf_yubo
-
 summary_path=$1
 model_dir=$2
+
+source /cpfs/data/shared/public/miniconda3/bin/activate
+conda activate lf_yubo
 
 bash eval_sinlge_card_1.sh $summary_path $model_dir&
 bash eval_sinlge_card_2.sh $summary_path $model_dir&
