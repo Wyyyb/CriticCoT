4bcd1067bd900e4ba72f3f3824f51412fc7f85c5
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-26 00:13:00 +0800

    add

--- exp_scripts_0125/train_qwen_32B_webinstruct_cft_80k_0125/train_0.sh
+++ exp_scripts_0125/train_qwen_32B_webinstruct_cft_80k_0125/train_0.sh
@@ -5,7 +5,7 @@ conda activate lf_yubo
 # cd /data/yubo/CriticCoT/LLaMA-Factory/
 cd /cpfs/data/user/yubowang/CriticCoT/LLaMA-Factory/
 PROJECT_NAME="critic_cot"
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 export WANDB_API_KEY=f318ffd0dcf5d31701fd33aee12e57e9cf15444f
 export WANDB_PROJECT=$PROJECT_NAME
 export WANDB_MODE=disabled
