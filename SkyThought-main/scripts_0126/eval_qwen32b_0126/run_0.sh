4cc711f80709274f291175c7dc2f2f673ab9eb85
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-26 22:34:36 +0800

    add

--- SkyThought-main/scripts_0126/eval_qwen32b_0126/run_0.sh
+++ SkyThought-main/scripts_0126/eval_qwen32b_0126/run_0.sh
@@ -4,6 +4,6 @@ source /gpfs/public/research/miniconda3/bin/activate
 conda activate lf_yubo
 
 cd /gpfs/public/research/xy/yubowang/CriticCoT/SkyThought-main/skythought/tools
-
+export CUDA_VISIBLE_DEVICES=0,1
 model_path="/gpfs/public/research/xy/yubowang/models/MAmmoTH-Critique-1"
-python eval.py --model ${model_path} --evals=AIME,MATH500,GPQADiamond --tp=4 --output_file=results.txt
+python eval.py --model ${model_path} --evals=AIME,MATH500,GPQADiamond --tp=2 --output_file=results.txt
