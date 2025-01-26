08323e88a3c876bf53c263e69f59842d0b7e1a85
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-26 23:33:00 +0800

    add

---
+++ exp_scritps_0126/eval_32B_models/eval_multi_0126_2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -ex
+
+source /gpfs/public/research/miniconda3/bin/activate
+conda activate lf_yubo
+
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+cd /gpfs/public/research/xy/yubowang/CriticCoT/exp_scritps_0126/eval_32B_models
+
+summary_path="/gpfs/public/research/xy/yubowang/CriticCoT/0126_eval_results_qwen2.5-32B_t2_critic_0126/summary.txt"
+model_dir="/gpfs/public/research/xy/yubowang/CriticCoT/LLaMA-Factory/output_models_0126/qwen2.5-32B_t2_critic_0126"
+
+bash eval_dir_models_math.sh $summary_path $model_dir
+
