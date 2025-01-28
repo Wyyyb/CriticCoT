5cee6a48d7cced98ee09217ff415c43f5094c78d
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-28 12:34:21 +0800

    add

--- SkyThought-main/scripts_0126/eval_sky_on_map_0128/run_0.sh
+++ SkyThought-main/scripts_0126/eval_sky_on_map_0128/run_0.sh
@@ -5,4 +5,4 @@ conda activate yubo_lf
 cd /map-vepfs/yubo/CriticCoT/SkyThought-main/skythought/tools
 export CUDA_VISIBLE_DEVICES=6,7
 model_path="/map-vepfs/yubo/models/Sky-T1-32B-Preview"
-python eval.py --model ${model_path} --evals=AIME,GPQADiamon --tp=2 --output_file="Sky-T1-32B-Preview_results.txt" --temperatures 0.7
+python eval.py --model ${model_path} --evals=GPQADiamond --tp=2 --output_file="Sky-T1-32B-Preview_results.txt" --temperatures 0.7
