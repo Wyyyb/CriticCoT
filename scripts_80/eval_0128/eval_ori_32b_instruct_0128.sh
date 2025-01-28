d4aeec6e0db6004ae8ad59c3e6e148b88117c073
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-28 18:56:35 +0800

    add

--- scripts_80/eval_0128/eval_ori_32b_instruct_0128.sh
+++ scripts_80/eval_0128/eval_ori_32b_instruct_0128.sh
@@ -4,7 +4,7 @@ set -ex
 #source /gpfs/public/research/miniconda3/bin/activate
 #conda activate lf_yubo
 
-export CUDA_VISIBLE_DEVICES=0,1,2,3
+export CUDA_VISIBLE_DEVICES=4,5,6,7
 # cd /gpfs/public/research/xy/yubowang/CriticCoT/exp_scritps_0126/eval_32B_models
 cd /data/yubo/CriticCoT/Qwen2.5-Math-Eval/evaluation/sh
 summary_path="/data/yubo/CriticCoT/0128_eval_results_Qwen2.5-32B-Instruct/summary.txt"
