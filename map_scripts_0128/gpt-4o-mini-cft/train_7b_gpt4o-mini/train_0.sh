01973ffb550bb6e5541024b62a8e742bcf631d83
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-28 11:28:46 +0800

    add

--- map_scripts_0128/gpt-4o-mini-cft/train_7b_gpt4o-mini/train_0.sh
+++ map_scripts_0128/gpt-4o-mini-cft/train_7b_gpt4o-mini/train_0.sh
@@ -2,7 +2,7 @@
 source /map-vepfs/miniconda3/bin/activate
 conda activate yubo_lf
 
-cd /data/yubo/CriticCoT/LLaMA-Factory/
+cd /map-vepfs/yubo/CriticCoT/LLaMA-Factory/
 PROJECT_NAME="critic_cot"
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 export WANDB_API_KEY=f318ffd0dcf5d31701fd33aee12e57e9cf15444f
