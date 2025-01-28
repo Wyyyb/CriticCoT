6fcfe89966cae0024f772e4255f219db6484a4a4
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-28 12:23:11 +0800

    add

--- map_scripts_0128/gpt-4o-mini-cft/eval_7b_gpt4o-mini/eval_dir_models_math_1.sh
+++ map_scripts_0128/gpt-4o-mini-cft/eval_7b_gpt4o-mini/eval_dir_models_math_1.sh
@@ -23,7 +23,7 @@ for checkpoint_dir in ${models_dir}/checkpoint-*; do
         checkpoint_num=$(basename "$checkpoint_dir" | cut -d'-' -f2)
 
         # 检查checkpoint_num是否[0, 30)
-        if [ "$checkpoint_num" -ge 2 ] && [ "$checkpoint_num" -lt 6 ]; then
+        if [ "$checkpoint_num" -ge 18 ] && [ "$checkpoint_num" -lt 28 ]; then
             # 设置输出目录
             output_dir="${summary_parent_dir}/${models_dir_name}-checkpoint-${checkpoint_num}/"
 
