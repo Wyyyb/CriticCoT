d4cb002076d2c1b3bd7c917b1c9c860a46f56a2d
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-29 18:25:00 +0800

    add

--- scripts_80/darth_eval_0129_cft_7b_best/eval_1.sh
+++ scripts_80/darth_eval_0129_cft_7b_best/eval_1.sh
@@ -7,9 +7,9 @@ set -ex
 export CUDA_VISIBLE_DEVICES=4
 # cd /gpfs/public/research/xy/yubowang/CriticCoT/exp_scritps_0126/eval_32B_models
 cd /data/yubowang/CriticCoT/Qwen2.5-Math-Eval/evaluation/sh
-summary_path="/data/yubowang/CriticCoT/0129_eval_results_CFT-Webinstruct-0121-ckpt-t_0_2_aime/summary.txt"
-output_dir="/data/yubowang/CriticCoT/0129_eval_results_CFT-Webinstruct-0121-ckpt-t_0_2_aime"
+summary_path="/data/yubowang/CriticCoT/0129_eval_results_CFT-Webinstruct-0121-ckpt-t_0_5_aime/summary.txt"
+output_dir="/data/yubowang/CriticCoT/0129_eval_results_CFT-Webinstruct-0121-ckpt-t_0_5_aime"
 checkpoint_dir="/data/yubowang/models/CFT-Webinstruct-0121-ckpt"
 
 #bash eval_compe.sh "$checkpoint_dir" "$output_dir" "$summary_path"
-bash eval_aime_temp.sh "$checkpoint_dir" "$output_dir" "$summary_path" 0.2
+bash eval_aime_temp.sh "$checkpoint_dir" "$output_dir" "$summary_path" 0.5
--- scripts_80/darth_eval_0129_cft_7b_best/eval_3.sh
+++ scripts_80/darth_eval_0129_cft_7b_best/eval_3.sh
@@ -7,9 +7,9 @@ set -ex
 export CUDA_VISIBLE_DEVICES=7
 # cd /gpfs/public/research/xy/yubowang/CriticCoT/exp_scritps_0126/eval_32B_models
 cd /data/yubowang/CriticCoT/Qwen2.5-Math-Eval/evaluation/sh
-summary_path="/data/yubowang/CriticCoT/0129_eval_results_CFT-Webinstruct-0121-ckpt-t_0_7_aime/summary.txt"
-output_dir="/data/yubowang/CriticCoT/0129_eval_results_CFT-Webinstruct-0121-ckpt-t_0_7_aime"
+summary_path="/data/yubowang/CriticCoT/0129_eval_results_CFT-Webinstruct-0121-ckpt-t_0_8_aime/summary.txt"
+output_dir="/data/yubowang/CriticCoT/0129_eval_results_CFT-Webinstruct-0121-ckpt-t_0_8_aime"
 checkpoint_dir="/data/yubowang/models/CFT-Webinstruct-0121-ckpt"
 
 #bash eval_compe.sh "$checkpoint_dir" "$output_dir" "$summary_path"
-bash eval_aime_temp.sh "$checkpoint_dir" "$output_dir" "$summary_path" 0.7
+bash eval_aime_temp.sh "$checkpoint_dir" "$output_dir" "$summary_path" 0.8
