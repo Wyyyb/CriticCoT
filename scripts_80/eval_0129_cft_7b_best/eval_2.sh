d404ff9904e8e9118ddc1491d6c3f95c117beb42
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-29 17:15:32 +0800

    add

--- scripts_80/eval_0129_cft_7b_best/eval_2.sh
+++ scripts_80/eval_0129_cft_7b_best/eval_2.sh
@@ -4,12 +4,12 @@ set -ex
 #source /gpfs/public/research/miniconda3/bin/activate
 #conda activate lf_yubo
 
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=2
 # cd /gpfs/public/research/xy/yubowang/CriticCoT/exp_scritps_0126/eval_32B_models
 cd /data/yubo/CriticCoT/Qwen2.5-Math-Eval/evaluation/sh
-summary_path="/data/yubo/CriticCoT/0129_eval_results_qwen2.5-7b-math-cft-gpt-4o-0128-t_0_2/summary.txt"
-output_dir="/data/yubo/CriticCoT/0129_eval_results_qwen2.5-7b-math-cft-gpt-4o-0128-t_0_2"
-checkpoint_dir="/data/yubo/models/qwen2.5-7b-math-cft-gpt-4o-0128"
+summary_path="/data/yubo/CriticCoT/0129_eval_results_CFT-Webinstruct-0121-ckpt-t_2/summary.txt"
+output_dir="/data/yubo/CriticCoT/0129_eval_results_CFT-Webinstruct-0121-ckpt-t_2"
+checkpoint_dir="/data/yubo/models/CFT-Webinstruct-0121-ckpt"
 
 #bash eval_compe.sh "$checkpoint_dir" "$output_dir" "$summary_path"
-bash eval_full.sh "$checkpoint_dir" "$output_dir" "$summary_path" 0.2
+bash eval_math_temp.sh "$checkpoint_dir" "$output_dir" "$summary_path" 0.2
