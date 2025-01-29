a5a57324c1c4e1d6d773e5e183078df2d5a4cd6d
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-29 13:51:30 +0800

    add

--- map_scripts_0128/best_7b_cft/eval_best_7b_0129/eval_dir_models_math_1.sh
+++ map_scripts_0128/best_7b_cft/eval_best_7b_0129/eval_dir_models_math_1.sh
@@ -23,7 +23,7 @@ for checkpoint_dir in ${models_dir}/checkpoint-*; do
         checkpoint_num=$(basename "$checkpoint_dir" | cut -d'-' -f2)
 
         # 检查checkpoint_num是否[0, 30)
-        if [ "$checkpoint_num" -ge 1 ] && [ "$checkpoint_num" -lt 3 ]; then
+        if [ "$checkpoint_num" -ge 16 ] && [ "$checkpoint_num" -lt 19 ]; then
             # 设置输出目录
             output_dir="${summary_parent_dir}/${models_dir_name}-checkpoint-${checkpoint_num}/"
 
--- map_scripts_0128/best_7b_cft/eval_best_7b_0129/eval_dir_models_math_2.sh
+++ map_scripts_0128/best_7b_cft/eval_best_7b_0129/eval_dir_models_math_2.sh
@@ -23,7 +23,7 @@ for checkpoint_dir in ${models_dir}/checkpoint-*; do
         checkpoint_num=$(basename "$checkpoint_dir" | cut -d'-' -f2)
 
         # 检查checkpoint_num是否[0, 30)
-        if [ "$checkpoint_num" -ge 3 ] && [ "$checkpoint_num" -lt 5 ]; then
+        if [ "$checkpoint_num" -ge 19 ] && [ "$checkpoint_num" -lt 22 ]; then
             # 设置输出目录
             output_dir="${summary_parent_dir}/${models_dir_name}-checkpoint-${checkpoint_num}/"
 
