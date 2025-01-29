318fad69046f86329e66af6076bf34dac6771aed
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-29 13:04:37 +0800

    add

---
+++ map_scripts_0128/best_7b_cft/eval_best_7b_0129/eval_7b_3.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -ex
+
+source /map-vepfs/miniconda3/bin/activate
+conda activate yubo_lf
+
+export CUDA_VISIBLE_DEVICES=2
+cd /map-vepfs/yubo/CriticCoT/map_scripts_0128/best_7b_cft/eval_best_7b_0129
+
+summary_path="/map-vepfs/yubo/CriticCoT/0128_eval_results_qwen2.5-math-7B-webinstruct_cft_80k_0121_p3_0129/summary.txt"
+model_dir="/map-vepfs/yubo/CriticCoT/LLaMA-Factory/output_models_0129/qwen2.5-math-7B-webinstruct_cft_80k_0121_p3_0129"
+
+bash eval_dir_models_math_3.sh $model_dir $summary_path
+
---
+++ map_scripts_0128/best_7b_cft/eval_best_7b_0129/start_6_cards.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -ex
+
+bash eval_7b_1.sh &
+bash eval_7b_2.sh &
+bash eval_7b_3.sh &
+bash eval_7b_4.sh &
+bash eval_7b_5.sh &
+bash eval_7b_6.sh
+
+
+
