cdb90ca154e6afdd1f775cbe13454ea59ba665c5
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-29 18:32:19 +0800

    add

--- scripts_80/darth_eval_0129_cft_7b_best/eval_0.sh
+++ scripts_80/darth_eval_0129_cft_7b_best/eval_0.sh
@@ -7,9 +7,9 @@ set -ex
 export CUDA_VISIBLE_DEVICES=3
 # cd /gpfs/public/research/xy/yubowang/CriticCoT/exp_scritps_0126/eval_32B_models
 cd /data/yubowang/CriticCoT/Qwen2.5-Math-Eval/evaluation/sh
-summary_path="/data/yubowang/CriticCoT/0129_eval_results_CFT-Webinstruct-0121-ckpt-t_0_4_aime/summary.txt"
-output_dir="/data/yubowang/CriticCoT/0129_eval_results_CFT-Webinstruct-0121-ckpt-t_0_4_aime"
+summary_path="/data/yubowang/CriticCoT/0129_eval_results_CFT-Webinstruct-0121-ckpt-t_0_9_aime/summary.txt"
+output_dir="/data/yubowang/CriticCoT/0129_eval_results_CFT-Webinstruct-0121-ckpt-t_0_9_aime"
 checkpoint_dir="/data/yubowang/models/CFT-Webinstruct-0121-ckpt"
 
 #bash eval_compe.sh "$checkpoint_dir" "$output_dir" "$summary_path"
-bash eval_aime_temp.sh "$checkpoint_dir" "$output_dir" "$summary_path" 0.4
+bash eval_aime_temp.sh "$checkpoint_dir" "$output_dir" "$summary_path" 0.9
