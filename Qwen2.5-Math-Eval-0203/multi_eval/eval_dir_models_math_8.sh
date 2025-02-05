07e4952d60d26f921a24f05d4cb6244f6d567e4d
Wyyyb <yubo.wang.sunny@gmail.com>
2025-02-05 21:44:13 +0800

    add

--- Qwen2.5-Math-Eval-0203/multi_eval/eval_dir_models_math_8.sh
+++ Qwen2.5-Math-Eval-0203/multi_eval/eval_dir_models_math_8.sh
@@ -22,7 +22,7 @@ for checkpoint_dir in ${models_dir}/checkpoint-*; do
         checkpoint_num=$(basename "$checkpoint_dir" | cut -d'-' -f2)
 
         # 检查checkpoint_num是否[0, 30)
-        if [ "$checkpoint_num" -ge 59 ] && [ "$checkpoint_num" -lt 100 ]; then
+        if [ "$checkpoint_num" -ge 59 ] && [ "$checkpoint_num" -lt 71 ]; then
             # 设置输出目录
             output_dir="${summary_parent_dir}/${models_dir_name}-checkpoint-${checkpoint_num}/"
 
