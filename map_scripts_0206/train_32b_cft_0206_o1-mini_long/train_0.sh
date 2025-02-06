b091a12e8440888926cd31fa6a0b09f3c801511f
Wyyyb <yubo.wang.sunny@gmail.com>
2025-02-06 17:30:18 +0800

    add

--- map_scripts_0206/train_32b_cft_0206_o1-mini_long/train_0.sh
+++ map_scripts_0206/train_32b_cft_0206_o1-mini_long/train_0.sh
@@ -9,7 +9,7 @@ export WANDB_API_KEY=f318ffd0dcf5d31701fd33aee12e57e9cf15444f
 export WANDB_PROJECT=$PROJECT_NAME
 # export WANDB_MODE=disabled
 export WANDB_DISABLED="true"
-export MASTER_PORT=$(shuf -i 20000-30000 -n 1)
+export MASTER_PORT=$(shuf -i 10000-20000 -n 1)
 export MASTER_ADDR="127.0.0.1"
 
 # 使用 torchrun 启动训练
