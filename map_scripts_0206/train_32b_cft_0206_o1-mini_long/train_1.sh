e1757f583cbadca34cccc59d48bfc385a0384620
Wyyyb <yubo.wang.sunny@gmail.com>
2025-02-06 17:56:58 +0800

    add

--- map_scripts_0206/train_32b_cft_0206_o1-mini_long/train_1.sh
+++ map_scripts_0206/train_32b_cft_0206_o1-mini_long/train_1.sh
@@ -4,6 +4,11 @@
 
 cd /map-vepfs/yubo/CriticCoT/LLaMA-Factory/
 PROJECT_NAME="critic_cot"
+
+export NCCL_P2P_DISABLE=1  # 禁用P2P通信
+export NCCL_IB_DISABLE=0   # 启用InfiniBand
+export CUDA_LAUNCH_BLOCKING=1  # 同步CUDA操作
+
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 export WANDB_API_KEY=f318ffd0dcf5d31701fd33aee12e57e9cf15444f
 export WANDB_PROJECT=$PROJECT_NAME
