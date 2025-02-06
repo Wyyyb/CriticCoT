1007c164c75e789ae1b5320ca005aa1810e267df
Wyyyb <yubo.wang.sunny@gmail.com>
2025-02-06 23:40:48 +0800

    add

--- Qwen2.5-Math-Eval-0203/multi_eval_32b/start_4_cards.sh
+++ Qwen2.5-Math-Eval-0203/multi_eval_32b/start_4_cards.sh
@@ -4,11 +4,11 @@ set -ex
 summary_path=$1
 model_dir=$2
 
-bash eval_7b_1.sh $summary_path $model_dir &
+
 bash eval_7b_2.sh $summary_path $model_dir &
 bash eval_7b_3.sh $summary_path $model_dir &
-bash eval_7b_4.sh $summary_path $model_dir
-
+bash eval_7b_4.sh $summary_path $model_dir &
+bash eval_7b_1.sh $summary_path $model_dir
 
 
 
