d0c1195be1d881b539a82deb17bd6d1eeee62865
Wyyyb <yubo.wang.sunny@gmail.com>
2025-02-06 16:33:06 +0800

    add

--- bespoke_data_process/filter_2k_bespoke_data_0206.py
+++ bespoke_data_process/filter_2k_bespoke_data_0206.py
@@ -9,6 +9,7 @@ def main():
     for each in ori_data:
         format_data.append(single_format(each))
     random.shuffle(format_data)
+    print("full data average length", sta_average_length(format_data))
     with open("../local_data/bespoke_data/bespoke_17k_format_data_0206.json", "w") as fo:
         fo.write(json.dumps(format_data, indent=4))
     format_data = sorted(format_data, key=lambda x: -len(x["answer"]))
@@ -16,6 +17,14 @@ def main():
     random.shuffle(selected_2k)
     with open("../local_data/bespoke_data/bespoke_2k_data_0206.json", "w") as fo:
         fo.write(json.dumps(selected_2k, indent=4))
+    print("selected_2k data average length", sta_average_length(selected_2k))
+
+
+def sta_average_length(input_list):
+    total_length = 0.0
+    for each in input_list:
+        total_length += len(each["answer"])
+    return total_length / len(input_list)
 
 
 def single_format(ori_single):
