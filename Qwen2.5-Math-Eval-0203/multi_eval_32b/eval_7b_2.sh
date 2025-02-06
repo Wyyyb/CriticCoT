02689d34fc7ff1e440c301aa25ad9c486994d1af
Wyyyb <yubo.wang.sunny@gmail.com>
2025-02-06 23:06:06 +0800

    add

--- Qwen2.5-Math-Eval-0203/multi_eval_32b/eval_7b_2.sh
+++ Qwen2.5-Math-Eval-0203/multi_eval_32b/eval_7b_2.sh
@@ -8,7 +8,7 @@ summary_path=$1
 model_dir=$2
 
 export CUDA_VISIBLE_DEVICES=2,3
-cd /map-vepfs/yubo/CriticCoT/Qwen2.5-Math-Eval-0203/multi_eval
+cd /map-vepfs/yubo/CriticCoT/Qwen2.5-Math-Eval-0203/multi_eval_32b
 
 #summary_path="/map-vepfs/yubo/CriticCoT/0203_eval_results_qwen2.5-math-7B-webinstruct_cft_40k_o1_mini_brief/summary.txt"
 #model_dir="/map-vepfs/yubo/CriticCoT/LLaMA-Factory/output_models_0203/qwen2.5-math-7B-webinstruct_cft_40k_o1_mini_brief"
