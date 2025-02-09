567044f0fd60c1c5f09da0535580a391dbb9cef2
Wyyyb <yubo.wang.sunny@gmail.com>
2025-02-09 17:25:57 +0800

    add

--- map_scripts_0209/7b_exp_0209/eval_0.sh
+++ map_scripts_0209/7b_exp_0209/eval_0.sh
@@ -5,10 +5,10 @@ conda activate yubo_lf
 summary_path="/map-vepfs/yubo/CriticCoT/0209_eval_results_qwen2.5-math-7b_webinstruct_cft_80k_0121_p3_exp0/summary.txt"
 model_dir="/map-vepfs/yubo/CriticCoT/LLaMA-Factory/output_models_0208/qwen2.5-math-7b_webinstruct_cft_80k_0121_p3_exp0"
 ckpt_start=0
-ckpt_end=52
+ckpt_end=16
 
 cd /map-vepfs/yubo/CriticCoT/Qwen2.5-Math-Eval-0203/multi_eval_0209_dense
 
-bash start_8_cards.sh $summary_path $model_dir $ckpt_start $ckpt_end
+#bash start_8_cards.sh $summary_path $model_dir $ckpt_start $ckpt_end
 
-
+bash eval_7b.sh $summary_path $model_dir $ckpt_start $ckpt_end
