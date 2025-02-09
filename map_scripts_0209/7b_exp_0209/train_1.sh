73e0c379a328667122ccf30ee072731b2d03a74e
Wyyyb <yubo.wang.sunny@gmail.com>
2025-02-09 11:59:28 +0800

    add

---
+++ map_scripts_0209/7b_exp_0209/train_1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source /map-vepfs/miniconda3/bin/activate
+conda activate yubo_lf
+
+cd /map-vepfs/yubo/CriticCoT/LLaMA-Factory/
+PROJECT_NAME="critic_cot"
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+export WANDB_API_KEY=f318ffd0dcf5d31701fd33aee12e57e9cf15444f
+export WANDB_PROJECT=$PROJECT_NAME
+# export WANDB_MODE=disabled
+export WANDB_DISABLED="true"
+export MASTER_PORT=$(shuf -i 30000-40000 -n 1)
+export MASTER_ADDR="127.0.0.1"
+
+FORCE_TORCHRUN=1 llamafactory-cli train ../map_scripts_0209/7b_exp_0209/qwen2.5-math-7b_0209_1.yaml
