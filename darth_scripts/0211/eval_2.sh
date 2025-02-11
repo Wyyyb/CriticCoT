e04c12d26699c6e6d600f96295ec7aad53619b20
Wyyyb <yubo.wang.sunny@gmail.com>
2025-02-11 13:21:55 +0800

    add

---
+++ darth_scripts/0211/eval_2.sh
@@ -0,0 +1,12 @@
+set -ex
+
+export CUDA_VISIBLE_DEVICES=6
+
+model_path="/data/yubowang/models/CFT-Webinstruct-0121-ckpt"
+output_dir="/data/yubowang/CriticCoT/eval_result_0211/t_0_0/"
+summary_path="/data/yubowang/CriticCoT/eval_result_0211/t_0_0/summary.txt"
+temp=0.2
+
+cd /data/yubowang/CriticCoT/Qwen2.5-Math-Eval-0203/scripts/
+bash eval_math_only.sh $model_path $output_dir $summary_path $temp
+
