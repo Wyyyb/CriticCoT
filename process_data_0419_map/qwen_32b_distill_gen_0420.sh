60ac599a07e634d4d1db5711ced9d57f15c4eb26
Wyyyb <yubo.wang.sunny@gmail.com>
2025-04-21 00:24:33 -0400

    add

--- process_data_0419_map/qwen_32b_distill_gen_0420.sh
+++ process_data_0419_map/qwen_32b_distill_gen_0420.sh
@@ -4,5 +4,5 @@ conda activate yubo_lf
 
 cd /map-vepfs/yubo/CriticCoT/process_data_0419_map
 
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 python qwen_32b_distill_gen_deepmath_solution_0420.py
