b674d700bc1846bdf260f1c27f33193acd35e82d
Wyyyb <yubo.wang.sunny@gmail.com>
2025-04-20 23:37:35 -0400

    add

--- process_data_0419/sbatch_32b_0420.sh
+++ process_data_0419/sbatch_32b_0420.sh
@@ -19,6 +19,7 @@ conda activate cft
 # 设置CUDA可见设备
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
+cd /mnt/hwfile/opendatalab/yubo/CriticCoT/process_data_0419/
 # 运行您的Python脚本，并将标准输出和标准错误重定向到指定文件
 python -u qwen_32b_gen_deepmath_solution_0420.py > qwen_32b_gen_deepmath_solution_0420_output.txt 2>&1
 
