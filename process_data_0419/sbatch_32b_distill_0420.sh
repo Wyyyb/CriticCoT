afb284ff3af8fead61b2cf573fcc5bae10cb2136
Wyyyb <yubo.wang.sunny@gmail.com>
2025-04-21 11:14:44 -0400

    add

--- process_data_0419/sbatch_32b_distill_0420.sh
+++ process_data_0419/sbatch_32b_distill_0420.sh
@@ -2,9 +2,9 @@
 #SBATCH --partition=belt_road       # 分区名称
 #SBATCH --nodes=1                   # 请求1个节点
 #SBATCH --ntasks-per-node=1         # 每节点1个任务
-#SBATCH --gres=gpu:2                # 请求2个GPU
-#SBATCH --mem=32G                   # 请求32GB内存
-#SBATCH --time=16:00:00             # 最长运行16小时
+#SBATCH --gres=gpu:8                # 请求2个GPU
+#SBATCH --mem=64G                   # 请求32GB内存
+#SBATCH --time=24:00:00             # 最长运行16小时
 #SBATCH --job-name=qwen_gen         # 作业名称
 #SBATCH --output=qwen_gen_%j.log    # Slurm的标准输出和错误日志，%j表示作业ID
 
@@ -18,7 +18,7 @@ conda activate cft
 # 不要手动设置CUDA_VISIBLE_DEVICES，让Slurm自动设置
 
 # 打印当前分配到的GPU设备
-export CUDA_VISIBLE_DEVICES=6,7
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
 # 运行Python脚本并实时输出到文件（禁用输出缓冲）
 python -u qwen_32b_distill_gen_deepmath_solution_0420.py > qwen_32b_distill_gen_deepmath_solution_0420_output.txt 2>&1
