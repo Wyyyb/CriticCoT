6820975b8a5df4571604a3f00bd99e565393bbc3
Wyyyb <yubo.wang.sunny@gmail.com>
2025-04-23 18:59:15 -0400

    add

--- Qwen2.5-Math-Eval-0203/utils.py
+++ Qwen2.5-Math-Eval-0203/utils.py
@@ -58,7 +58,7 @@ def load_prompt(data_name, prompt_type, num_shots):
 
     if data_name in ["gsm_hard", "svamp", "tabmwp", "asdiv", "mawps"]:
         data_name = "gsm8k"
-    if data_name in ["math_oai", "hungarian_exam", "aime24", "amc23", "math-500"]:
+    if data_name in ["math_oai", "hungarian_exam", "aime24", "aime25", "amc23", "math-500"]:
         data_name = "math"
     if data_name in ["sat_math"]:
         data_name = "mmlu_stem"
---
+++ pjlab_scritps_0423/eval_aime/eval_aime25_0.sh
@@ -0,0 +1,16 @@
+
+source /mnt/petrelfs/wangyubo.p/miniconda3/etc/profile.d/conda.sh
+conda activate cft
+
+summary_path="eval_results_0423/qwen_32b_aime25/summary.txt"
+model_path="/mnt/hwfile/opendatalab/yubo/models/Qwen-2.5-32B"
+output_path="eval_results_0423/qwen_32b_aime25/"
+
+mkdir -p output_path
+
+cd /mnt/hwfile/opendatalab/yubo/CriticCoT/Qwen2.5-Math-Eval-0203/scripts
+
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+
+bash evaluate_qwen_test.sh $model_path $output_path $summary_path
+
