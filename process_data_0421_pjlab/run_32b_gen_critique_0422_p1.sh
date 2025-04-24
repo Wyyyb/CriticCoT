f049c63e0d7fed171b1dd5f21cb789c6962971f7
Wyyyb <yubo.wang.sunny@gmail.com>
2025-04-24 00:58:35 -0400

    add

--- process_data_0421_pjlab/run_32b_gen_critique_0422_p1.sh
+++ process_data_0421_pjlab/run_32b_gen_critique_0422_p1.sh
@@ -17,10 +17,10 @@ source /mnt/petrelfs/wangyubo.p/miniconda3/etc/profile.d/conda.sh
 conda activate cft
 
 # 设置CUDA可见设备
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
-# export CUDA_VISIBLE_DEVICES=0,1,2,3
+#export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 
 cd /mnt/hwfile/opendatalab/yubo/CriticCoT/process_data_0421_pjlab
 # 运行您的Python脚本，并将标准输出和标准错误重定向到指定文件
-python -u qwen_32b_distill_gen_critique_0422.py > qwen_32b_distill_gen_critique_0424_output.txt 2>&1
-# python -u qwen_32b_distill_gen_critique_0422.py
+#python -u qwen_32b_distill_gen_critique_0422.py > qwen_32b_distill_gen_critique_0424_output.txt 2>&1
+python -u qwen_32b_distill_gen_critique_0422.py
