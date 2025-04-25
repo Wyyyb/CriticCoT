40d6848ce05c31837ce08dfd8f0bac3633bb32ea
Wyyyb <yubo.wang.sunny@gmail.com>
2025-04-25 12:30:31 -0400

    add

---
+++ Qwen2.5-Math-Eval-0203/multi_eval_0425/eval_7b_3.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -ex
+
+#source /map-vepfs/miniconda3/bin/activate
+#conda activate yubo_lf
+
+summary_path=$1
+model_dir=$2
+start=$3
+end=$4
+
+export CUDA_VISIBLE_DEVICES=2
+#cd /map-vepfs/yubo/CriticCoT/Qwen2.5-Math-Eval-0203/multi_eval_0209_dense
+cd /mnt/hwfile/opendatalab/yubo/CriticCoT/Qwen2.5-Math-Eval-0203/multi_eval_0425
+#summary_path="/map-vepfs/yubo/CriticCoT/0203_eval_results_qwen2.5-math-7B-webinstruct_cft_40k_o1_mini_brief/summary.txt"
+#model_dir="/map-vepfs/yubo/CriticCoT/LLaMA-Factory/output_models_0203/qwen2.5-math-7B-webinstruct_cft_40k_o1_mini_brief"
+
+bash eval_dir_models_math.sh $model_dir $summary_path $start $end
---
+++ pjlab_scritps_0423/eval_0425_32b/eval_0.sh
@@ -0,0 +1,14 @@
+
+source /mnt/petrelfs/wangyubo.p/miniconda3/etc/profile.d/conda.sh
+conda activate cft
+
+summary_path="/mnt/hwfile/opendatalab/yubo/CriticCoT/0425_eval_results_32b_cft_deepmath/summary.txt"
+model_dir="/mnt/hwfile/opendatalab/yubo/CriticCoT/360-LLaMA-Factory-sp/output_models_0425/deepmath_qwen_32b_distill_cft_0424"
+ckpt_start=0
+ckpt_end=160
+
+cd /mnt/hwfile/opendatalab/yubo/CriticCoT/Qwen2.5-Math-Eval-0203/multi_eval_0425
+
+bash start_8_cards.sh $summary_path $model_dir $ckpt_start $ckpt_end
+
+# bash eval_7b.sh $summary_path $model_dir $ckpt_start $ckpt_end
