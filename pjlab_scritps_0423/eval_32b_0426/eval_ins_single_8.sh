4ce13c3f981ca450841522738076cebca661fd71
Wyyyb <yubo.wang.sunny@gmail.com>
2025-04-27 03:02:04 -0400

    add

--- pjlab_scritps_0423/eval_32b_0426/eval_ins_single_8.sh
+++ pjlab_scritps_0423/eval_32b_0426/eval_ins_single_8.sh
@@ -16,9 +16,9 @@ conda activate cft
 summary_path="/mnt/hwfile/opendatalab/yubo/CriticCoT/0426_eval_results_32b_instruct_cft_deepmath/summary.txt"
 models_dir="/mnt/hwfile/opendatalab/yubo/CriticCoT/360-LLaMA-Factory-sp/output_models_0426/deepmath_qwen_32b_instruct_distill_cft_0425"
 start=0
-end=250
+end=330
 
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+export CUDA_VISIBLE_DEVICES=0,1,6,7
 
 cd /mnt/hwfile/opendatalab/yubo/CriticCoT/Qwen2.5-Math-Eval-0203/multi_eval_0425
 
