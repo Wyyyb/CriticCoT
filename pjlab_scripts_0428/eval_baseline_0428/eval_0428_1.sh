335ece06f5c5f77393b0d199ebbe4945dcce5a6e
Wyyyb <yubo.wang.sunny@gmail.com>
2025-04-28 23:00:48 -0400

    add

---
+++ pjlab_scripts_0428/eval_baseline_0428/eval_0428_1.sh
@@ -0,0 +1,20 @@
+
+source /mnt/petrelfs/wangyubo.p/miniconda3/etc/profile.d/conda.sh
+conda activate cft
+
+summary_path="../baseline_eval_results_0428_1/qwen_32b_distill/summary.txt"
+model_path="/mnt/hwfile/opendatalab/yubo/models/DeepSeek-R1-Distill-Qwen-32B"
+output_path="../baseline_eval_results_0428_1/qwen_32b_distill/"
+
+mkdir -p $output_path
+
+cd /mnt/hwfile/opendatalab/yubo/CriticCoT/Qwen2.5-Math-Eval-0203/scripts
+
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+
+bash evaluate_distilled_qwen.sh $model_path $output_path $summary_path
+
+
+
+
+
