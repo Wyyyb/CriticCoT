2297a0fe214c36803a941264702f8b3cb8364b05
Wyyyb <yubo.wang.sunny@gmail.com>
2025-04-29 18:44:36 -0400

    add

--- process_data_0429_darth/qwen3-32b_gen_critique_0429.sh
+++ process_data_0429_darth/qwen3-32b_gen_critique_0429.sh
@@ -5,8 +5,8 @@
 #conda activate cft
 
 # 设置CUDA可见设备
-# export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
-export CUDA_VISIBLE_DEVICES=0,1,2,3
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+# export CUDA_VISIBLE_DEVICES=0,1,2,3
 
 cd /data/yubowang/CriticCoT/process_data_0429_darth
 # 运行您的Python脚本，并将标准输出和标准错误重定向到指定文件
