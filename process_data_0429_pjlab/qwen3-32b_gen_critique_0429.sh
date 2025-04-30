b214d731b6b1f90864cbf1d2d01af69465e819fa
Wyyyb <yubo.wang.sunny@gmail.com>
2025-04-30 03:00:24 -0400

    add

--- process_data_0429_pjlab/qwen3-32b_gen_critique_0429.sh
+++ process_data_0429_pjlab/qwen3-32b_gen_critique_0429.sh
@@ -19,5 +19,5 @@ export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
 cd /data/yubowang/CriticCoT/process_data_0429_pjlab
 # 运行您的Python脚本，并将标准输出和标准错误重定向到指定文件
-python -u qwen3_32b_gen_critique_0429.py
+python -u qwen3_32b_gen_critique_0429.py > qwen3_32b_gen_critique_0429_output_p1.txt 2>&1
 
