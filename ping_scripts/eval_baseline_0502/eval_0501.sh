54e9ca004c7841e049a98c138ed280be4390d479
Wyyyb <yubo.wang.sunny@gmail.com>
2025-05-02 16:21:31 -0400

    add

--- ping_scripts/eval_baseline_0502/eval_0501.sh
+++ ping_scripts/eval_baseline_0502/eval_0501.sh
@@ -1,29 +1,29 @@
 
-source /mnt/petrelfs/wangyubo.p/miniconda3/etc/profile.d/conda.sh
+source /data/yubo/miniconda3/etc/profile.d/conda.sh
 conda activate cft
 
 summary_path="../baseline_eval_results_0430/Qwen3-8B-Base/summary.txt"
-model_path="/mnt/hwfile/opendatalab/yubo/models/Qwen3-8B-Base"
+model_path="/data/yubo/models/Qwen3-8B-Base"
 output_path="../baseline_eval_results_0430/Qwen3-8B-Base/"
 
 mkdir -p $output_path
 
-cd /mnt/hwfile/opendatalab/yubo/CriticCoT/Qwen2.5-Math-Eval-0203/scripts
+cd /data/yubo/CriticCoT/Qwen2.5-Math-Eval-0203/scripts
 
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+export CUDA_VISIBLE_DEVICES=6,7
 
 bash evaluate_distilled_qwen.sh $model_path $output_path $summary_path
 
 
 summary_path="../baseline_eval_results_0430/Qwen3-8B/summary.txt"
-model_path="/mnt/hwfile/opendatalab/yubo/models/Qwen3-8B"
+model_path="/data/yubo/models/Qwen3-8B"
 output_path="../baseline_eval_results_0430/Qwen3-8B/"
 
 mkdir -p $output_path
 
-cd /mnt/hwfile/opendatalab/yubo/CriticCoT/Qwen2.5-Math-Eval-0203/scripts
+cd /data/yubo/CriticCoT/Qwen2.5-Math-Eval-0203/scripts
 
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+export CUDA_VISIBLE_DEVICES=6,7
 
 bash evaluate_distilled_qwen.sh $model_path $output_path $summary_path
 
@@ -32,27 +32,27 @@ source /mnt/petrelfs/wangyubo.p/miniconda3/etc/profile.d/conda.sh
 conda activate cft
 
 summary_path="../baseline_eval_results_0430/Qwen3-14B-Base/summary.txt"
-model_path="/mnt/hwfile/opendatalab/yubo/models/Qwen3-14B-Base"
+model_path="/data/yubo/models/Qwen3-14B-Base"
 output_path="../baseline_eval_results_0430/Qwen3-14B-Base/"
 
 mkdir -p $output_path
 
-cd /mnt/hwfile/opendatalab/yubo/CriticCoT/Qwen2.5-Math-Eval-0203/scripts
+cd /data/yubo/CriticCoT/Qwen2.5-Math-Eval-0203/scripts
 
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+export CUDA_VISIBLE_DEVICES=6,7
 
 bash evaluate_distilled_qwen.sh $model_path $output_path $summary_path
 
 
 summary_path="../baseline_eval_results_0430/Qwen3-14B/summary.txt"
-model_path="/mnt/hwfile/opendatalab/yubo/models/Qwen3-14B"
+model_path="/data/yubo/models/Qwen3-14B"
 output_path="../baseline_eval_results_0430/Qwen3-14B/"
 
 mkdir -p $output_path
 
-cd /mnt/hwfile/opendatalab/yubo/CriticCoT/Qwen2.5-Math-Eval-0203/scripts
+cd /data/yubo/CriticCoT/Qwen2.5-Math-Eval-0203/scripts
 
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+export CUDA_VISIBLE_DEVICES=6,7
 
 bash evaluate_distilled_qwen.sh $model_path $output_path $summary_path
 
