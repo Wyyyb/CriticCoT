47eb68b8dac81e4756285cc8e538d97a7c999d11
Wyyyb <yubo.wang.sunny@gmail.com>
2025-05-02 17:49:26 -0400

    add

--- pjlab_run_scripts_0501/run_1.sh
+++ pjlab_run_scripts_0501/run_1.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-cd /mnt/hwfile/opendatalab/yubo/CriticCoT/process_data_0429_pjlab/
-bash run_1_qwen3_gen_corrction_0501.sh
+#cd /mnt/hwfile/opendatalab/yubo/CriticCoT/process_data_0429_pjlab/
+#bash run_1_qwen3_gen_corrction_0501.sh
+
+cd /mnt/hwfile/opendatalab/yubo/CriticCoT/process_data_0502_pjlab/
+bash sbatch_qwen3_32b_geb_solution.sh
+
+
