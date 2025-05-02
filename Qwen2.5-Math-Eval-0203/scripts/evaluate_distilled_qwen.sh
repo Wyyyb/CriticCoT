f991f1052fb2eb34fa8dbb70610254c82fd74af1
Wyyyb <yubo.wang.sunny@gmail.com>
2025-05-01 20:22:55 -0400

    add

--- Qwen2.5-Math-Eval-0203/scripts/evaluate_distilled_qwen.sh
+++ Qwen2.5-Math-Eval-0203/scripts/evaluate_distilled_qwen.sh
@@ -10,7 +10,7 @@ NUM_TEST_SAMPLE=-1
 mkdir -p $OUTPUT_DIR
 cd ..
 
-DATA_NAME="aime25,aime24,math,amc23,minerva_math,olympiadbench,gsm8k"
+DATA_NAME="aime25,aime24,math-500,amc23,minerva_math,olympiadbench,gsm8k"
 # DATA_NAME="minerva_math"
 TOKENIZERS_PARALLELISM=false \
 python3 -u math_eval.py \
