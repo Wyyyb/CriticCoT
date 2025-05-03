c15482a1b6ff33676deaebbb51d93bca24de1178
Wyyyb <yubo.wang.sunny@gmail.com>
2025-05-02 22:58:52 -0400

    add

---
+++ ping_scripts/eval_baseline_0502/test_0502_aime24_re.sh
@@ -0,0 +1,33 @@
+
+source /data/yubo/miniconda3/etc/profile.d/conda.sh
+conda activate cft
+#
+#summary_path="../baseline_eval_results_0430/Qwen3-8B-Base/summary.txt"
+#model_path="/data/yubo/models/Qwen3-8B-Base"
+#output_path="../baseline_eval_results_0430/Qwen3-8B-Base/"
+#
+#mkdir -p $output_path
+#
+#cd /data/yubo/CriticCoT/Qwen2.5-Math-Eval-0203/scripts
+#
+#export CUDA_VISIBLE_DEVICES=6,7
+#
+#bash evaluate_distilled_qwen.sh $model_path $output_path $summary_path
+
+
+summary_path="../baseline_eval_results_0430/Qwen3-8B/summary.txt"
+model_path="/data/yubo/models/Qwen3-8B"
+output_path="../baseline_eval_results_0430/Qwen3-8B/"
+
+mkdir -p $output_path
+
+cd /data/yubo/CriticCoT/Qwen2.5-Math-Eval-0203/scripts
+
+export CUDA_VISIBLE_DEVICES=4,5
+
+bash evaluate_distilled_qwen.sh $model_path $output_path $summary_path
+
+
+
+
+
