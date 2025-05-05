21c803e90a1f2334c325c5f446783a79c65ecbf2
Wyyyb <yubo.wang.sunny@gmail.com>
2025-05-05 00:14:25 -0400

    add

--- ping_scripts/eval_baseline_0503/eval_mimo.sh
+++ ping_scripts/eval_baseline_0503/eval_mimo.sh
@@ -12,7 +12,7 @@ cd /data/yubo/CriticCoT/Qwen2.5-Math-Eval-0203/scripts
 
 export CUDA_VISIBLE_DEVICES=4
 
-bash evaluate_qwen3_8_re.sh $model_path $output_path $summary_path
+bash evaluate_qwen3.sh $model_path $output_path $summary_path
 
 
 summary_path="../baseline_eval_results_0503/MiMo-7B-SFT/summary.txt"
@@ -25,7 +25,7 @@ cd /data/yubo/CriticCoT/Qwen2.5-Math-Eval-0203/scripts
 
 export CUDA_VISIBLE_DEVICES=4
 
-bash evaluate_qwen3_8_re.sh $model_path $output_path $summary_path
+bash evaluate_qwen3.sh $model_path $output_path $summary_path
 
 
 
