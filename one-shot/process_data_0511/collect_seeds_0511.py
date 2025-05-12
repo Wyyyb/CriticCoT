ca6a697100a4923e7fb0683ae7128752cb65c934
y726wang <ubo.wang.sunny@gmail.com>
2025-05-11 23:14:30 -0400

    debug

--- one-shot/process_data_0511/collect_seeds_0511.py
+++ one-shot/process_data_0511/collect_seeds_0511.py
@@ -22,6 +22,7 @@ def get_deepmath_seeds(input_path):
         if "qwen-2.5-32b_answer_correctness" not in v:
             continue
         v["data_source"] = "deepmath"
+        v = post_process_item(v)
         if v["qwen-2.5-32b_answer_correctness"] is True:
             right_candidates.append(v)
         elif v["qwen-2.5-32b_answer_correctness"] is False:
@@ -41,6 +42,7 @@ def get_webinstruct_v_seeds(input_path):
         if "qwen3-32b_extracted_answer" not in v:
             continue
         v["data_source"] = "webinstruct_verified"
+        v = post_process_item(v)
         if v["qwen3-32b_extracted_answer"] == v["gt_answer"]:
             right_candidates.append(v)
         elif v["qwen3-32b_extracted_answer"] != v["gt_answer"]:
