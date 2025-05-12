e43f2e358a3b3eceb2c5269e105dfeb2ebf9871f
y726wang <ubo.wang.sunny@gmail.com>
2025-05-12 19:43:40 -0400

    add

--- one-shot/process_data_0511/merge_add_solution_0512.py
+++ one-shot/process_data_0511/merge_add_solution_0512.py
@@ -3,7 +3,7 @@ import os
 
 
 def merge(input_path_1, input_path_2, output_path):
-    add_keys = ["MiMo-7B-SFT"]
+    add_keys = ["MiMo-7B-SFT", "Qwen3-8B"]
     with open(input_path_1, "r") as f1, open(input_path_2, "r") as f2:
         input_data_1 = json.load(f1)
         input_data_2 = json.load(f2)
