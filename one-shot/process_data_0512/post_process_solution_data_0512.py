0f2abffb290da2a33cc97eeba1b0cf37d69bc7b2
y726wang <ubo.wang.sunny@gmail.com>
2025-05-12 18:21:47 -0400

    add

--- one-shot/process_data_0512/post_process_solution_data_0512.py
+++ one-shot/process_data_0512/post_process_solution_data_0512.py
@@ -6,7 +6,7 @@ def process(input_path, output_path):
     with open(input_path, "r") as fi:
         data = json.load(fi)
     critique_id = 0
-    output_data = []
+    output_data = {}
     sta_map = {}
     for k, v in data.items():
         for each_k, each_v in v["student_solutions"].items():
@@ -21,7 +21,7 @@ def process(input_path, output_path):
                 curr["student_solution"] = solution
                 curr["critique_id"] = str(critique_id)
                 critique_id += 1
-                output_data.append(curr)
+                output_data[curr["critique_id"]] = curr
     print("len(output_data)", len(output_data))
     print("sta_map", sta_map)
     with open(output_path, "w") as fo:
