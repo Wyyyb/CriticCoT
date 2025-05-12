4424a29178f8a9bf545f2213b88dba0349ee7e7b
y726wang <ubo.wang.sunny@gmail.com>
2025-05-12 14:36:23 -0400

    debug

--- one-shot/process_data_0511/add_solution_darth_0512_1.sh
+++ one-shot/process_data_0511/add_solution_darth_0512_1.sh
@@ -2,4 +2,4 @@ export CUDA_VISIBLE_DEVICES=4,5,6,7
 
 cd /data/yubowang/CriticCoT/one-shot/process_data_0511
 
-python add_student_solution_darth_0512.py
+python add_student_solution_darth_0512_1.py
