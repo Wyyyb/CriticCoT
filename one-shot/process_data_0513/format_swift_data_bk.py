a64cdaa644c278cd85336bfea66f8836bc86e426
y726wang <ubo.wang.sunny@gmail.com>
2025-05-13 15:09:25 -0400

    add

--- one-shot/process_data_0513/format_swift_data_bk.py
+++ one-shot/process_data_0513/format_swift_data_bk.py
@@ -2,23 +2,8 @@ import json
 import os
 
 
-def data_filter(item):
-    critique = item.get("critique", None)
-    if not critique:
-        return {}
-    if "verified_as_wrong" in critique and critique["verified_as_wrong"] is True:
-        return {}
-    if "</think>" in critique:
-        critique = critique.split("</think>")[-1]
-    item["critique"] = critique
-    return item
-
-
 def format_single(item):
     instruction = "Please critique whether the following solution to the question is correct.\n\n"
-    item = data_filter(item)
-    if not item:
-        return None
     question = item.get("question", None)
     solution = item.get("student_solution", {}).get("solution", None)
     critique = item.get("critique", None)
@@ -43,7 +28,9 @@ def main(input_dir, output_path):
             continue
         file_path = os.path.join(input_dir, file)
         with open(file_path, "r") as f:
-            input_data = json.load(f)
+            data = json.load(f)
+        for k, v in data.items():
+            input_data.append(v)
     output_data = []
     for each in input_data:
         curr = format_single(each)
@@ -58,7 +45,7 @@ def main(input_dir, output_path):
 
 if __name__ == "__main__":
     input_dir_path = "../../local_data/one_shot_data_0513/"
-    output_file_path = "../../local_data/training_data_0513/full_one-shot_train_data_rm_thinking_0513.jsonl"
+    output_file_path = "../../local_data/training_data_0513/full_one-shot_train_data_0513.jsonl"
     os.makedirs("../../local_data/training_data_0513/", exist_ok=True)
     main(input_dir_path, output_file_path)
 
