e09e3266c1f47e694816839f362da0191c90e478
y726wang <ubo.wang.sunny@gmail.com>
2025-05-14 13:02:54 -0400

    add

--- ping_scripts/eval_one-shot_0513/eval_qwen_2-5_math_5_1.sh
+++ ping_scripts/eval_one-shot_0513/eval_qwen_2-5_math_5_1.sh
@@ -2,7 +2,6 @@
 
 # 激活conda环境
 # conda activate cft
-bash eval_qwen_2-5_math_5_1.sh
 
 # 设置要处理的检查点数字列表
 # checkpoint_numbers=(2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32 34 36 38 40)
@@ -15,7 +14,7 @@ export CUDA_VISIBLE_DEVICES=0,1,2,3
 # 串行处理每个检查点，每次都使用全部4张卡
 for ckpt_num in "${checkpoint_numbers[@]}"; do
     summary_path="../eval_results_0514_qwen3-8b_1/summary.txt"
-    model_path="/data/yubo/CriticCoT/ms-swift/output_models_0514_qwen3-8b_one_shot_balance/v1-20250514-155509/checkpoint-${ckpt_num}"
+    model_path="/data/yubo/CriticCoT/ms-swift/output_models_0514_qwen3-8b_one_shot_balance_1/v1-20250514-160221/checkpoint-${ckpt_num}"
     output_path="../eval_results_0514_qwen3-8b_1/balance_one-shot_exp_ckpt_${ckpt_num}/"
 
     echo "Processing checkpoint ${ckpt_num}"
