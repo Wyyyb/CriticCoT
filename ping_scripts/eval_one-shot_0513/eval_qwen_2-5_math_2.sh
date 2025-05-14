0aa4f000f17e928ae24751adbbc204cf4bc6f809
y726wang <ubo.wang.sunny@gmail.com>
2025-05-13 23:41:52 -0400

    add

--- ping_scripts/eval_one-shot_0513/eval_qwen_2-5_math_2.sh
+++ ping_scripts/eval_one-shot_0513/eval_qwen_2-5_math_2.sh
@@ -4,14 +4,14 @@
 conda activate cft
 
 # 设置要处理的检查点数字列表
-checkpoint_numbers=(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72)
+checkpoint_numbers=(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64)
 
  # 使用所有4张GPU
-export CUDA_VISIBLE_DEVICES=6,7
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 
 # 串行处理每个检查点，每次都使用全部4张卡
 for ckpt_num in "${checkpoint_numbers[@]}"; do
-    summary_path="../eval_results_0513_2/one-shot_exp_ckpt_${ckpt_num}/summary.txt"
+    summary_path="../eval_results_0513_2/summary.txt"
     model_path="/data/yubo/CriticCoT/ms-swift/output_models_0513_filtered_full_38k/v2-20250514-010049/checkpoint-${ckpt_num}"
     output_path="../eval_results_0513_2/one-shot_exp_ckpt_${ckpt_num}/"
 
