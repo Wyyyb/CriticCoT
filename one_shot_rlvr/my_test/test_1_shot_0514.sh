18ea998bfde3034ec338b24198f8c7fa29b05ef8
y726wang <ubo.wang.sunny@gmail.com>
2025-05-14 19:42:34 -0400

    add

--- one_shot_rlvr/my_test/test_1_shot_0514.sh
+++ one_shot_rlvr/my_test/test_1_shot_0514.sh
@@ -3,6 +3,7 @@ export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 export CHECKPOINTS_DIR=./checkpoints/ # your checkpoint path
 
 conda activate rlvr_train
+cd ../
 bash scripts/train/training_1.5b_pi1_r128.sh
 
 
