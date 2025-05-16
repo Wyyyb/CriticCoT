dc3fa8c2514cca5cba04d4ce8e21bfc551304159
y726wang <ubo.wang.sunny@gmail.com>
2025-05-16 00:27:36 -0400

    add

---
+++ ping_scripts/eval_think_0515/eval_distill_1-5b_think.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# 激活conda环境
+# conda activate cft
+
+# 设置要处理的检查点数字列表
+checkpoint_numbers=(2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32 34 36 38 40)
+# checkpoint_numbers=(2 4 6 8 10 12 14 16 18 20)
+# checkpoint_numbers=(4 8 12 16 20 24 28 32 36 40)
+
+ # 使用所有4张GPU
+export CUDA_VISIBLE_DEVICES=0,1
+
+# 串行处理每个检查点，每次都使用全部4张卡
+for ckpt_num in "${checkpoint_numbers[@]}"; do
+    summary_path="../eval_results_0514_distill_7b_think_0/summary.txt"
+    model_path="/data/yubo/CriticCoT/ms-swift/output_models_dsr_distill_1.5b_0515_think_p0/v0-20250516-042304/checkpoint-${ckpt_num}"
+    output_path="../eval_results_0514_distill_7b_think_0/ckpt_${ckpt_num}/"
+
+    echo "Processing checkpoint ${ckpt_num}"
+
+    cd /data/yubo/CriticCoT/Qwen2.5-Math-Eval-0203/scripts
+    mkdir -p $output_path
+
+    bash evaluate_qwen3.sh $model_path $output_path $summary_path
+
+    echo "Finished processing checkpoint ${ckpt_num}"
+done
+
+echo "All checkpoints processed successfully!"
