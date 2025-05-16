6a0c841004253c2d37add6a69d7bae1fb9d4dd6f
y726wang <ubo.wang.sunny@gmail.com>
2025-05-16 15:29:40 -0400

    add

---
+++ Qwen2.5-Math-Eval-0203/scripts/evaluate_llama.sh
@@ -0,0 +1,55 @@
+set -ex
+
+PROMPT_TYPE="llama"
+MODEL_NAME_OR_PATH=$1
+OUTPUT_DIR=$2
+SUMMARY_PATH=$3
+SPLIT="test"
+NUM_TEST_SAMPLE=-1
+
+mkdir -p $OUTPUT_DIR
+cd ..
+
+# DATA_NAME="aime25,aime24,math-500,minerva_math,olympiadbench,amc23,theoremqa,gsm8k"
+# DATA_NAME="minerva_math"
+DATA_NAME="aime25,aime24,math-500,minerva_math,amc23,olympiadbench"
+TOKENIZERS_PARALLELISM=false \
+python3 -u math_eval.py \
+    --model_name_or_path ${MODEL_NAME_OR_PATH} \
+    --data_name ${DATA_NAME} \
+    --output_dir ${OUTPUT_DIR} \
+    --summary_path ${SUMMARY_PATH} \
+    --split ${SPLIT} \
+    --prompt_type ${PROMPT_TYPE} \
+    --num_test_sample ${NUM_TEST_SAMPLE} \
+    --seed 0 \
+    --temperature 0 \
+    --max_tokens_per_call 4096 \
+    --n_sampling 1 \
+    --top_p 1 \
+    --start 0 \
+    --end -1 \
+    --use_vllm \
+    --save_outputs \
+    # --overwrite \
+
+
+#DATA_NAME="aime24"
+#TOKENIZERS_PARALLELISM=false \
+#python3 -u math_eval.py \
+#    --model_name_or_path ${MODEL_NAME_OR_PATH} \
+#    --data_name ${DATA_NAME} \
+#    --output_dir ${OUTPUT_DIR} \
+#    --summary_path ${SUMMARY_PATH} \
+#    --split ${SPLIT} \
+#    --prompt_type ${PROMPT_TYPE} \
+#    --num_test_sample ${NUM_TEST_SAMPLE} \
+#    --seed 0 \
+#    --temperature 0.8 \
+#    --n_sampling 1 \
+#    --top_p 1 \
+#    --start 0 \
+#    --end -1 \
+#    --use_vllm \
+#    --save_outputs \
+#    # --overwrite \
---
+++ ping_scripts/eval_dsr_0516/eval_llama_dsr_p0.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# 激活conda环境
+# conda activate cft
+
+# 设置要处理的检查点数字列表
+checkpoint_numbers=(2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32 34 36 38 40)
+# checkpoint_numbers=(2 4 6 8 10 12 14 16 18 20)
+# checkpoint_numbers=(4 8 12 16 20 24 28 32 36 40)
+
+ # 使用所有4张GPU
+export CUDA_VISIBLE_DEVICES=2,3
+
+# 串行处理每个检查点，每次都使用全部4张卡
+for ckpt_num in "${checkpoint_numbers[@]}"; do
+    summary_path="../eval_results_llama_dsr_0516_p0/summary.txt"
+    model_path="/data/yubo/CriticCoT/ms-swift/output_models_dsr_llama-3b_ins_0516_p0/v0-20250516-192104/checkpoint-${ckpt_num}"
+    output_path="../eval_results_llama_dsr_0516_p0/llama_dsr_p0_ckpt_${ckpt_num}/"
+
+    echo "Processing checkpoint ${ckpt_num}"
+
+    cd /data/yubo/CriticCoT/Qwen2.5-Math-Eval-0203/scripts
+    mkdir -p $output_path
+
+    bash evaluate_llama.sh $model_path $output_path $summary_path
+
+    echo "Finished processing checkpoint ${ckpt_num}"
+done
+
+echo "All checkpoints processed successfully!"
