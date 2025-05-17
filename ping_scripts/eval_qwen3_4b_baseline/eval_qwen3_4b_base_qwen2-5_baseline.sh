613d310b6043a92eec0feaf2971296c0ca1aeebe
y726wang <ubo.wang.sunny@gmail.com>
2025-05-17 00:28:48 -0400

    add

--- ping_scripts/eval_qwen3_4b_baseline/eval_qwen3_4b_base_qwen2-5_baseline.sh
+++ ping_scripts/eval_qwen3_4b_baseline/eval_qwen3_4b_base_qwen2-5_baseline.sh
@@ -4,9 +4,9 @@ conda activate cft
 
 export CUDA_VISIBLE_DEVICES=4,7
 
-summary_path="../baseline_eval_results_0517/summary.txt"
+summary_path="../baseline_eval_results_0517_2-5/summary.txt"
 model_path="/data/yubo/models/Qwen3-4B-Base"
-output_path="../baseline_eval_results_0517/Qwen3-4B-Base_qwen2-5/"
+output_path="../baseline_eval_results_0517_2-5/Qwen3-4B-Base_qwen2-5/"
 
 cd /data/yubo/CriticCoT/Qwen2.5-Math-Eval-0203/scripts
 
