c4d2ba0323034da944859ebd8b75f69be1098de8
y726wang <ubo.wang.sunny@gmail.com>
2025-05-17 02:06:01 -0400

    add

--- ping_scripts/eval_qwen3_4b_baseline/eval_qwen3_4b_base_qwen3_baseline_3334.sh
+++ ping_scripts/eval_qwen3_4b_baseline/eval_qwen3_4b_base_qwen3_baseline_3334.sh
@@ -13,3 +13,26 @@ cd /data/yubo/CriticCoT/Qwen2.5-Math-Eval-0203/scripts
 mkdir -p $output_path
 
 bash evaluate_qwen3.sh $model_path $output_path $summary_path
+
+
+summary_path="../baseline_eval_results_0517_2-5/summary.txt"
+model_path="/data/yubo/models/Qwen3-8B-Base"
+output_path="../baseline_eval_results_0517_2-5/Qwen3-8B-Base_qwen2-5/"
+
+cd /data/yubo/CriticCoT/Qwen2.5-Math-Eval-0203/scripts
+
+mkdir -p $output_path
+
+bash evaluate_qwen.sh $model_path $output_path $summary_path
+
+
+summary_path="../baseline_eval_results_0517/summary.txt"
+model_path="/data/yubo/models/Qwen3-8B-Base"
+output_path="../baseline_eval_results_0517/Qwen3-8B-Base_qwen3/"
+
+cd /data/yubo/CriticCoT/Qwen2.5-Math-Eval-0203/scripts
+
+mkdir -p $output_path
+
+bash evaluate_qwen3.sh $model_path $output_path $summary_path
+
