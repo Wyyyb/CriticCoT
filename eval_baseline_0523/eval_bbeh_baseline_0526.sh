b06163a7cd8eefdfa9be40af0210935702dc5ed4
y726wang <ubo.wang.sunny@gmail.com>
2025-05-26 02:21:03 -0400

    add

--- eval_baseline_0523/eval_bbeh_baseline_0526.sh
+++ eval_baseline_0523/eval_bbeh_baseline_0526.sh
@@ -1,4 +1,16 @@
 
+summary_path="../../baseline_eval_results_0523/qwen3_4b/summary.txt"
+model_path="/data/yubowang/models/Qwen3-4B"
+output_path="../../baseline_eval_results_0523/qwen3_4b/"
+
+cd /data/yubowang/CriticCoT/bbeh/bbeh
+
+mkdir -p $output_path
+
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+
+bash eval_qwen3.sh $model_path $output_path $summary_path
+
 
 summary_path="../../baseline_eval_results_0523/Qwen2.5-Math-7B/summary.txt"
 model_path="/data/yubowang/models/Qwen2.5-Math-7B"
@@ -8,7 +20,7 @@ cd /data/yubowang/CriticCoT/bbeh/bbeh
 
 mkdir -p $output_path
 
-export CUDA_VISIBLE_DEVICES=6,7
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 
 bash eval_qwen2-5.sh $model_path $output_path $summary_path
 
@@ -21,7 +33,6 @@ cd /data/yubowang/CriticCoT/bbeh/bbeh
 
 mkdir -p $output_path
 
-export CUDA_VISIBLE_DEVICES=6,7
 
 bash eval_qwen2-5.sh $model_path $output_path $summary_path
 
@@ -35,7 +46,6 @@ cd /data/yubowang/CriticCoT/bbeh/bbeh
 
 mkdir -p $output_path
 
-export CUDA_VISIBLE_DEVICES=6,7
 
 bash eval_qwen2-5.sh $model_path $output_path $summary_path
 
@@ -48,13 +58,33 @@ cd /data/yubowang/CriticCoT/bbeh/bbeh
 
 mkdir -p $output_path
 
-export CUDA_VISIBLE_DEVICES=6,7
-
 bash eval_qwen2-5.sh $model_path $output_path $summary_path
 
 
+summary_path="../../baseline_eval_results_0523/Qwen3-4B-Base/summary.txt"
+model_path="/data/yubowang/models/Qwen3-4B-Base"
+output_path="../../baseline_eval_results_0523/Qwen3-4B-Base/"
+
+cd /data/yubowang/CriticCoT/bbeh/bbeh
+
+mkdir -p $output_path
+
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+
+bash eval_qwen3.sh $model_path $output_path $summary_path
 
 
+summary_path="../../baseline_eval_results_0523/Qwen3-8B-Base/summary.txt"
+model_path="/data/yubowang/models/Qwen3-8B-Base"
+output_path="../../baseline_eval_results_0523/Qwen3-8B-Base/"
+
+cd /data/yubowang/CriticCoT/bbeh/bbeh
+
+mkdir -p $output_path
+
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+
+bash eval_qwen3.sh $model_path $output_path $summary_path
 
 
 
