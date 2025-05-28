0dfabdf3ff01209c2be0368c90481a50e9769175
y726wang <ubo.wang.sunny@gmail.com>
2025-05-28 15:06:35 -0400

    add

--- bbeh_exp_0528/map_scripts_0528/eval_qwen25_math_easy_causal_understanding_p1.sh
+++ bbeh_exp_0528/map_scripts_0528/eval_qwen25_math_easy_causal_understanding_p1.sh
@@ -1,10 +1,11 @@
 
 source /map-vepfs/miniconda3/bin/activate
-conda activate ys_swift
+conda activate yubo_eval
 
 base_dir="/map-vepfs/yubo/CriticCoT/ms-swift/output_models_0528_bbeh_qwen_2-5_math_7b_easy_causal_understanding_p1/v0-20250528-183855"
-output_base_dir="../../baseline_eval_results_0528"
+output_base_dir="../../eval_results_0528_easy_causal_understanding_p1"
 model_name="Qwen2.5-Math-7B"
+task_list="bbeh_causal_understanding,bbeh_disambiguation_qa,bbeh_boolean_expressions,bbeh_time_arithmetic,bbeh_buggy_tables,bbeh_object_counting,bbeh_zz_mini"
 
 cd /data/yubo/CriticCoT/bbeh/bbeh
 
@@ -14,24 +15,24 @@ export CUDA_VISIBLE_DEVICES=4,5,6,7
 for i in $(seq 2 2 50); do
   checkpoint="checkpoint-$i"
   model_path="$base_dir/$checkpoint"
-  
+
   # 创建对应的输出目录
   output_path="$output_base_dir/$model_name/$checkpoint"
   summary_path="$output_path/summary.txt"
-  
+
   # 确保输出目录存在
   mkdir -p $output_path
-  
+
   echo "==========================================================="
   echo "Evaluating $checkpoint"
   echo "Model path: $model_path"
   echo "Output path: $output_path"
   echo "Summary path: $summary_path"
   echo "==========================================================="
-  
+
   # 运行评估脚本
-  bash eval_qwen2-5.sh $model_path $output_path $summary_path
-  
+  bash eval_qwen2-5_task.sh $model_path $output_path $summary_path $task_list
+
   # 可选：添加间隔时间，避免资源冲突
   sleep 5
 done
