aae85881888b80ba2e79021f9e3fa211706da704
y726wang <ubo.wang.sunny@gmail.com>
2025-05-28 16:31:32 -0400

    add

--- bbeh_exp_0528/ping_scripts_0528/eval_qwen25_math_easy_causal_understanding_p0.sh
+++ bbeh_exp_0528/ping_scripts_0528/eval_qwen25_math_easy_causal_understanding_p0.sh
@@ -10,7 +10,7 @@ task_list="bbeh_causal_understanding,bbeh_zz_mini"
 
 cd /data/yubo/CriticCoT/bbeh/bbeh
 
-export CUDA_VISIBLE_DEVICES=0,1,2,3
+export CUDA_VISIBLE_DEVICES=4,5,6,7
 
 # 循环处理checkpoint-2到checkpoint-50，步长为2
 for i in $(seq 8 2 50); do
