30dbc16893b0c1f36707e72dc210bf37c18bfa99
y726wang <ubo.wang.sunny@gmail.com>
2025-05-29 15:07:38 -0400

    add

---
+++ bbeh_exp_0528/map_scripts_0529/eval_qwen25_math_7b_3-tasks-merge_p0.sh
@@ -0,0 +1,40 @@
+
+source /map-vepfs/miniconda3/bin/activate
+conda activate yubo_eval
+
+base_dir="/map-vepfs/yubo/CriticCoT/ms-swift/output_models_0529_bbeh_qwen3_4b_base_3-tasks-merge_p0/v1-20250529-182439"
+output_base_dir="../../eval_results_0529_qwen3_4b_3-tasks-merge_p0"
+model_name="Qwen2.5-Math-7B"
+# task_list="bbeh_causal_understanding,bbeh_disambiguation_qa,bbeh_boolean_expressions,bbeh_time_arithmetic,bbeh_buggy_tables,bbeh_object_counting,bbeh_zz_mini"
+task_list="bbeh_causal_understanding,bbeh_disambiguation_qa,bbeh_time_arithmetic,bbeh_zz_mini"
+
+cd /map-vepfs/yubo/CriticCoT/bbeh/bbeh
+
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+
+# 循环处理checkpoint-2到checkpoint-50，步长为2
+for i in $(seq 10 10 150); do
+  checkpoint="checkpoint-$i"
+  model_path="$base_dir/$checkpoint"
+
+  # 创建对应的输出目录
+  output_path="$output_base_dir/$model_name/$checkpoint"
+  summary_path="$output_base_dir/$model_name/summary.txt"
+
+  # 确保输出目录存在
+  mkdir -p $output_path
+
+  echo "==========================================================="
+  echo "Evaluating $checkpoint"
+  echo "Model path: $model_path"
+  echo "Output path: $output_path"
+  echo "Summary path: $summary_path"
+  echo "==========================================================="
+
+  # 运行评估脚本
+  bash eval_qwen2-5_task.sh $model_path $output_path $summary_path $task_list
+
+  # 可选：添加间隔时间，避免资源冲突
+  sleep 5
+done
+
---
+++ bbeh_exp_0528/map_scripts_0529/train_qwen25_math_7b_3_tasks_merge_p0_0529.sh
@@ -0,0 +1,80 @@
+#!/usr/bin/env bash
+set -x
+
+source /map-vepfs/miniconda3/bin/activate
+conda activate ys_swift
+
+MODEL_PATH="/map-vepfs/yubo/models/Qwen2.5-Math-7B"
+
+DATA_PATH="/map-vepfs/yubo/CriticCoT/bbeh_exp_0528/training_data_0528/bbeh_one-shot_train_data_0528-3-tasks-merge_p0.jsonl"
+
+OUTPUT_DIR="/map-vepfs/yubo/CriticCoT/ms-swift/output_models_0529_bbeh_qwen25_math_3-tasks-merge_p0/"
+
+if [ ! -d "$OUTPUT_DIR" ]; then
+  mkdir -p "$OUTPUT_DIR"
+fi
+
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+
+cd /map-vepfs/yubo/CriticCoT/ms-swift
+
+torchrun \
+    --nproc_per_node 8 \
+    --standalone \
+    swift/cli/sft.py\
+    --use_hf True \
+    \
+    --model $MODEL_PATH \
+    --train_type full \
+    --torch_dtype bfloat16 \
+    \
+    --dataset $DATA_PATH \
+    --split_dataset_ratio 0 \
+    --dataset_num_proc 8 \
+    --streaming False \
+    --strict False \
+    --deepspeed zero3 \
+    --remove_unused_columns False \
+    --dataloader_num_workers 8 \
+    \
+    --truncation_strategy delete \
+    \
+    --output_dir $OUTPUT_DIR \
+    --gradient_checkpointing True \
+    --per_device_train_batch_size 1 \
+    --weight_decay 0.05 \
+    --learning_rate 5e-6 \
+    --lr_scheduler_type "cosine" \
+    --report_to none \
+    --logging_first_step True \
+    --logging_steps 1 \
+    \
+    --num_train_epochs 50 \
+    --gradient_accumulation_steps 64 \
+    --save_strategy "steps" \
+    --save_steps 10 \
+    --save_only_model True \
+    --warmup_ratio 0.05 \
+    --ddp_backend "nccl" \
+    \
+    --freeze_llm False \
+    --freeze_vit False \
+    --freeze_aligner False\
+    --attn_impl flash_attn \
+
+    # --attn_impl flash_attn \
+
+    # --save_strategy "epoch" \
+    # --save_strategy "steps" \
+    # --save_steps 109 \
+    # --save_total_limit 5 \
+    # --deepspeed zero3 \
+    # --max_steps -1 \
+    # --device_map auto \
+    # --override_exist_file True \
+    # --eval_strategy None \
+    # --custom_dataset_in
+
+cd /map-vepfs/yubo/CriticCoT/bbeh_exp_0528/map_scripts_0529
+
+bash eval_qwen25_math_7b_3-tasks-merge_p0.sh
