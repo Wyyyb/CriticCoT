db08cb5c8277ea0fb013bf75357b5082fee4182a
y726wang <ubo.wang.sunny@gmail.com>
2025-05-29 14:25:56 -0400

    add

--- bbeh_exp_0528/map_scripts_0529/eval_qwen3_4b_3-tasks-merge_p0.sh
+++ bbeh_exp_0528/map_scripts_0529/eval_qwen3_4b_3-tasks-merge_p0.sh
@@ -2,7 +2,7 @@
 source /map-vepfs/miniconda3/bin/activate
 conda activate yubo_eval
 
-base_dir="/map-vepfs/yubo/CriticCoT/ms-swift/output_models_0529_bbeh_qwen3_4b_base_3-tasks-merge_p0/v0-20250529-182039"
+base_dir="/map-vepfs/yubo/CriticCoT/ms-swift/output_models_0529_bbeh_qwen3_4b_base_3-tasks-merge_p0/v1-20250529-182439"
 output_base_dir="../../eval_results_0529_qwen3_4b_3-tasks-merge_p0"
 model_name="Qwen3-4B-Base"
 # task_list="bbeh_causal_understanding,bbeh_disambiguation_qa,bbeh_boolean_expressions,bbeh_time_arithmetic,bbeh_buggy_tables,bbeh_object_counting,bbeh_zz_mini"
