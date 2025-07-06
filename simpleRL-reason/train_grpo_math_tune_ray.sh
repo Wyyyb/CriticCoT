1669425819ca29523d16d104db1bffaa093e6f37
y726wang <ubo.wang.sunny@gmail.com>
2025-07-05 20:05:18 -0400

    add

--- simpleRL-reason/train_grpo_math_tune_ray.sh
+++ simpleRL-reason/train_grpo_math_tune_ray.sh
@@ -174,7 +174,7 @@ ray job submit --address=${HEAD_IP}:${HEAD_PORT} \
     }' \
   -- python -m verl.trainer.main_ppo \
   algorithm.adv_estimator=grpo \
-  data.train_files=$HDFS_DATA_PATH/$DATASET_NAME/deepscaler_train_filter.jsonl \
+  data.train_files=$HDFS_DATA_PATH/$DATASET_NAME/deepscaler_train_filter.json \
   data.val_files=$HDFS_DATA_PATH/$DATASET_NAME/test.parquet \
   data.train_batch_size=$TRAIN_BATCH_SIZE \
   data.val_batch_size=$VAL_BATCH_SIZE \
