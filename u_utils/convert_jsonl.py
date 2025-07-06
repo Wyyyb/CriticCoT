b2097ca2ccd50e73d9dda6802072f400e0a505a4
y726wang <ubo.wang.sunny@gmail.com>
2025-07-05 20:01:09 -0400

    add

--- u_utils/convert_jsonl.py
+++ u_utils/convert_jsonl.py
@@ -1,23 +1,22 @@
 import json
+import pandas as pd
 
 # 读取第一个文件并打印长度
-with open("deepscaler_critique_formatted.json", "r") as fi:
+with open("../verl_data/training_data/deepscaler_critique_formatted/deepscaler_critique_formatted.json", "r") as fi:
     critique_data = json.load(fi)
     print(len(critique_data))
 
 # 读取第二个文件并打印长度
-with open("deepscaler_train_filter.json", "r") as fi:
+with open("../verl_data/training_data/deepscaler_train_filter/deepscaler_train_filter.json", "r") as fi:
     train_data = json.load(fi)
     print(len(train_data))
 
-# 将第一个文件转换为JSONL格式
-with open("deepscaler_critique_formatted.jsonl", "w") as fo:
-    for item in critique_data:
-        fo.write(json.dumps(item, ensure_ascii=False) + "\n")
+# 将第一个文件转换为DataFrame，然后保存为Parquet格式
+critique_df = pd.DataFrame(critique_data)
+critique_df.to_parquet("../verl_data/training_data/deepscaler_critique_formatted/deepscaler_critique_formatted.parquet", index=False)
 
-# 将第二个文件转换为JSONL格式
-with open("deepscaler_train_filter.jsonl", "w") as fo:
-    for item in train_data:
-        fo.write(json.dumps(item, ensure_ascii=False) + "\n")
+# 将第二个文件转换为DataFrame，然后保存为Parquet格式
+train_df = pd.DataFrame(train_data)
+train_df.to_parquet("../verl_data/training_data/deepscaler_train_filter/deepscaler_train_filter.parquet", index=False)
 
 print("转换完成！")
