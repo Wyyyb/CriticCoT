043e3cb8178396a864a6fef718c637a974c57bee
y726wang <ubo.wang.sunny@gmail.com>
2025-07-11 22:04:51 -0400

    add

--- sweswe-main/docker/build_docker_verl_yb.sh
+++ sweswe-main/docker/build_docker_verl_yb.sh
@@ -7,6 +7,6 @@ VERSION=0.3
 
 # cd ./math_serving/reasonreason && git checkout main && git pull && cd -
 
-sudo docker build --network host -t harbor.xaminim.com/minimax-dialogue/$TASK:${VERSION} . -f docker/Dockerfile_verl_yb
+sudo docker build --network host -t harbor.xaminim.com/minimax-dialogue/$TASK:${VERSION} . -f Dockerfile_verl_yb
 
 sudo docker push harbor.xaminim.com/minimax-dialogue/$TASK:${VERSION}
